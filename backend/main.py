e10251518335766b7174e00951157d84b6d615ec
Nadim Abrar <nadimabrar@gmail.com>
2021-07-03 01:56:16 +0600

    Test change to trigger workflow for backend API

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Multiplyr Backend Says: Hello World"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
