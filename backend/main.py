88cfd59c66e1164ec51530705872766d0e2a3f89
Nadim Abrar <nadimabrar@gmail.com>
2021-07-03 02:49:20 +0600

    Revert test multipyr backend message change

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Multiplyr Backend Says: Hello World"}
+    return {"message": "Hello World"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
