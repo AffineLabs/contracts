d883b8d732fe46d84e0f423142755427957996d8
Tarik Moon <tarik@tarikmoon.com>
2021-07-07 01:24:09 -0700

    fixing libs and general setup

--- web-frontend/src/index.js
+++ web-frontend/src/index.js
@@ -2,26 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-// import { store } from "./store/store";
-// import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap";
-// import "jquery/dist/jquery.js";
 
 // TODO: use when we use Redux
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
