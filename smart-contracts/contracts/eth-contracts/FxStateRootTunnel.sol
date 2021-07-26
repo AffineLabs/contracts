05083dac6f98a4bbdc4f4d540ef61e053603b1c5
Nadim Abrar <nadimabrar@gmail.com>
2021-07-26 21:42:11 +0600

    Add initial smart contract hardhat setup along with eth polygon state transfer smart contracts

---
+++ smart-contracts/contracts/eth-contracts/FxStateRootTunnel.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.3;
+
+import { FxBaseRootTunnel } from '../tunnel/FxBaseRootTunnel.sol';
+
+/** 
+ * @title FxStateRootTunnel
+ */
+contract FxStateRootTunnel is FxBaseRootTunnel {
+    bytes public latestData;
+
+    constructor(address _checkpointManager, address _fxRoot)  FxBaseRootTunnel(_checkpointManager, _fxRoot) {}
+
+    function _processMessageFromChild(bytes memory data) internal override {
+        latestData = data;
+    }
+
+    function sendMessageToChild(bytes memory message) public {
+        _sendMessageToChild(message);
+    }
+
+    function getLatestReceivedDataFromChild() public returns (bytes memory) {
+        return latestData;
+    }
+}
