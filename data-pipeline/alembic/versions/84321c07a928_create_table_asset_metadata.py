d8c1509ae2023609c9d6df2bb445887bd39ca09c
Temp Collab <nzxcvb5@gmail.com>
2021-07-29 11:44:30 -0400

    created intial almbic config, and tables asset_price, asset_daily_metrics and asset_metadata

---
+++ data-pipeline/alembic/versions/84321c07a928_create_table_asset_metadata.py
@@ -0,0 +1,33 @@
+"""create table asset_metadata
+
+Revision ID: 84321c07a928
+Revises: c7099efc574c
+Create Date: 2021-07-28 12:07:41.079193
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "84321c07a928"
+down_revision = "c7099efc574c"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "asset_metadata",
+        sa.Column("asset_id", sa.Integer, primary_key=True),
+        sa.Column("asset_ticker", sa.String(64), nullable=False),
+        sa.Column("asset_name", sa.String(256), nullable=False),
+        sa.Column("asset_type", sa.String(64), nullable=False),
+        sa.Column("asset_description", sa.String(256), nullable=False),
+        sa.Column("asset_img_url", sa.String(256), nullable=True),
+        sa.Column("asset_url", sa.String(256), nullable=True),
+    )
+
+
+def downgrade():
+    op.drop_table("asset_metadata")
