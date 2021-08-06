8084f188e165dbe6c4a6928079d45485b223fbf0
Mahi Shafiullah <mahi@cs.nyu.edu>
2021-08-06 16:33:07 -0400

    Added S3 utilities and cron jobs in the data pipeline

---
+++ data-pipeline/data-collection-pipeline/cron_jobs/get_asset_prices.py
@@ -0,0 +1,31 @@
+import logging
+import sys
+import os.path
+sys.path.append("..")
+
+from cron_utils import write_to_file, upload_to_s3
+from scrapers.cefi_scraper import CeFiScraper
+
+TEMP_LOCAL_SAVE_DIR = "test_results"
+S3_BUCKET_FOR_API_DATA = "testbucketforapidata"
+
+assets_of_interest = ["SPY", 
+                      "QQQ",
+                      "BND"]
+
+scraper = CeFiScraper(assets=assets_of_interest)
+try:
+    scraper.collect_data()
+except:
+    logging.warning("Asset price collection failed.")
+
+all_asset_data = scraper.data
+
+for asset in assets_of_interest:
+    written_file_path = write_to_file(all_asset_data[asset], file_name=asset, save_dir=TEMP_LOCAL_SAVE_DIR)
+    logging.info("Written ", written_file_path)
+    success = upload_to_s3(S3_BUCKET_FOR_API_DATA,
+                           written_file_path,
+                           f"{asset}/{os.path.basename(written_file_path)}")
+    if not success:
+        logging.warning(f"S3 upload failed for {asset}")
---
+++ data-pipeline/data-collection-pipeline/cron_jobs/get_crypto_prices.py
@@ -0,0 +1,35 @@
+import logging
+import sys
+import os.path
+sys.path.append("..")
+
+from cron_utils import write_to_file, upload_to_s3
+from scrapers.coingecko_scraper import CoinGeckoScraper
+
+TEMP_LOCAL_SAVE_DIR = "test_results"
+S3_BUCKET_FOR_API_DATA = "testbucketforapidata"
+
+coins_of_interest = {
+    'BTC': 'bitcoin',
+    'ETH': 'ethereum',
+    'USDT': 'tether',
+    'USDC': 'usd-coin',
+    'BNB': 'binancecoin',
+    'UST': 'terrausd',
+    'DAI': 'dai',
+}
+
+crypto_price_data = CoinGeckoScraper(assets=coins_of_interest.values()).get_data()
+
+jsonable_dict = {}
+for coin_ticker in coins_of_interest:
+    jsonable_dict[coin_ticker] = crypto_price_data[coins_of_interest[coin_ticker]]
+    written_file_path = write_to_file(jsonable_dict[coin_ticker], 
+                                      file_name=coin_ticker, 
+                                      save_dir=TEMP_LOCAL_SAVE_DIR)
+    logging.info("Written ", written_file_path)
+    success = upload_to_s3(S3_BUCKET_FOR_API_DATA,
+                            written_file_path,
+                            f"{coin_ticker}/{os.path.basename(written_file_path)}")
+    if not success:
+        logging.warning(f"S3 upload failed for {coin_ticker}")
