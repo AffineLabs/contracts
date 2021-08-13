8c5fa895c9286835c3e38eef49c6b3699dd780e9
Mahi Shafiullah <mahi@cs.nyu.edu>
2021-08-13 18:53:21 -0400

    Modified the docker target to run the cron jobs

--- data-pipeline/run.sh
+++ data-pipeline/run.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 echo "running data collection pipeline"
-python3 "./data-collection-pipeline/data_pipeline.py"
+python3 "./data-collection-pipeline/cron_jobs/get_asset_prices.py"
+python3 "./data-collection-pipeline/cron_jobs/get_crypto_prices.py"
 
 echo "running data preprocessing pipeline"
 python3 "./data-preprocessing-pipeline/main.py"
