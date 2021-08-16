04de8f75a5e951c418937c0376f02354273cab37
Mahi Shafiullah <mahi@cs.nyu.edu>
2021-08-16 15:25:36 -0400

    Added pipfile req, updated run

--- data-pipeline/run.sh
+++ data-pipeline/run.sh
@@ -3,6 +3,8 @@
 echo "running data collection pipeline"
 python3 "./data-collection-pipeline/cron_jobs/get_asset_prices.py"
 python3 "./data-collection-pipeline/cron_jobs/get_crypto_prices.py"
+python3 "./data-collection-pipeline/cron_jobs/get_amm_data.py"
+python3 "./data-collection-pipeline/cron_jobs/get_lending_returns.py"
 
 echo "running data preprocessing pipeline"
 python3 "./data-preprocessing-pipeline/main.py"
