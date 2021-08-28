854cf442ba6be5edc07f2a738971b5f3efb94ac0
Adib Hasan <nzxcvb5@gmail.com>
2021-08-28 01:11:22 -0700

    added portfolio analysis with mixing of different asset class level portfolios

--- data-pipeline/run.sh
+++ data-pipeline/run.sh
@@ -3,9 +3,16 @@
 echo "running data collection pipeline"
 cd "./data-collection-pipeline/cron_jobs/"
 
-python3 "get_asset_prices.py"
+# echo "scrapping asset prices"
+# python3 "get_asset_prices.py"
+
+echo "getting coin prices"
 python3 "get_crypto_prices.py"
+
+echo "getting amm data"
 python3 "get_amm_data.py"
+
+echo "getting lending protocol returns"
 python3 "get_lending_returns.py"
 
 echo "running data preprocessing pipeline"
