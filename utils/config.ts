8f587b25679f64576a74307bfa3f6ac758e2869d
Nadim Abrar <nadimabrar@gmail.com>
2022-01-13 11:57:39 +0600

    Remove non sensitive config values from .env file

--- utils/config.ts
+++ utils/config.ts
@@ -1,35 +1,22 @@
-import { assert } from "chai";
 import { config as dotenvConfig } from "dotenv";
 import { resolve } from "path";
 import { address } from "./types";
 
 dotenvConfig({ path: resolve(__dirname, "./.env") });
 
-const ETH_USDC = process.env.ETH_USDC || "";
-const POLYGON_USDC = process.env.POLYGON_USDC || "";
-const POLYGON_ERC20_PREDICATE = process.env.POLYGON_ERC20_PREDICATE || "";
-const ROOT_CHAIN_MANAGER = process.env.ROOT_CHAIN_MANAGER || "";
-const DEFENDER = process.env.DEFENDER || "";
-const ETH_WORMHOLE = process.env.ETH_WORMHOLE || "";
-const POLYGON_WORMHOLE = process.env.POLYGON_WORMHOLE || "";
-
-assert(ETH_USDC !== "", "ETH USDC Address is needed for testing. Please set ETH_USDC in the .env file.");
-assert(POLYGON_USDC !== "", "Polygon USDC Address is needed for testing. Please set POLYGON_USDC in the .env file.");
-assert(
-  POLYGON_ERC20_PREDICATE !== "",
-  "Polygon ERC20 predicate address is needed for testing. Please set POLYGON_ERC20_PREDICATE in the .env file.",
-);
-
-assert(DEFENDER !== "", "Defender address is needed for testing. Please set DEFENDER in the .env file.");
-assert(ETH_WORMHOLE !== "" && POLYGON_WORMHOLE !== "", "Add wormhole addresses.");
-assert(ROOT_CHAIN_MANAGER !== "", "Set ETH chain manager");
+// Hardcoded Mumbai Addresses
+const ETH_USDC = "0x077ffc33b12ac8CFfF5B9F71658bc6575E16a113";
+const POLYGON_USDC = "0x5fD6A096A23E95692E37Ec7583011863a63214AA";
+const POLYGON_ERC20_PREDICATE = "0x37c3bfC05d5ebF9EBb3FF80ce0bd0133Bf221BC8";
+const ROOT_CHAIN_MANAGER = "0xBbD7cBFA79faee899Eaf900F13C9065bF03B1A74";
+const ETH_WORMHOLE = "0x706abc4E45D419950511e474C7B9Ed348A4a716c";
+const POLYGON_WORMHOLE = "0x0CBE91CF822c73C2315FB05100C2F714765d5c20";
 
 export interface Config {
   l1ChainManager: address;
   l1USDC: address;
   l2USDC: address;
   l2ERC20Predicate: address;
-  defender: address;
   l1worm: address;
   l2worm: address;
 }
@@ -38,7 +25,6 @@ export const config: Config = {
   l1USDC: ETH_USDC,
   l2USDC: POLYGON_USDC,
   l2ERC20Predicate: POLYGON_ERC20_PREDICATE,
-  defender: DEFENDER,
   l1worm: ETH_WORMHOLE,
   l2worm: POLYGON_WORMHOLE,
 };
@@ -51,11 +37,11 @@ export interface RebalanceConfig {
   l2VaultAddr: address;
 }
 
-const ethAlchemyURL = process.env.ETH_ALCHEMY_URL || "";
-const polygonAlchemyURL = process.env.POLYGON_ALCHEMY_URL || "";
+const ethAlchemyURL = process.env.ALCHEMY_ETH_KEY ? `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_ETH_KEY}` || "";
+const polygonAlchemyURL = process.env.ALCHEMY_POLYGON_KEY ? `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_POLYGON_KEY}` || "";
 const mnemonic = process.env.MNEMONIC || "";
-const l1VaultAddr = process.env.L1_VAULT_ADDR || "";
-const l2VaultAddr = process.env.L2_VAULT_ADDR || "";
+const l1VaultAddr = "0xe05F99bd5B4f755Caf9bd5E46bDd1468F9D650Fa";
+const l2VaultAddr = "0xd914975c045f2d29770C11656bCe4236aF3Dfe19";
 
 export const REBALANCE_CONFIG: RebalanceConfig = {
   ethAlchemyURL,
