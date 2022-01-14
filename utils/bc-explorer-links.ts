4f7e52a81278dd9ffd4558bf51ac9434d5dbf790
larryob <9146641+larryob@users.noreply.github.com>
2022-01-14 14:35:02 -0500

    Add solhint. Prettify everything

--- utils/bc-explorer-links.ts
+++ utils/bc-explorer-links.ts
@@ -1,23 +1,27 @@
-import { Contract, ContractTransaction } from "ethers"
+import { Contract, ContractTransaction } from "ethers";
 
 const NETWORK_TO_EXPLORER_URL = new Map([
-    ["ethMainnet", "mainnet.etherscan.io"],
-    ["ethGoerli", "goerli.etherscan.io"],
-    ["ethKovan", "kovan.etherscan.io"],
-    ["ethRinkeby", "rinkeby.etherscan.io"],
-    ["ethRopsten", "ropsten.etherscan.io"],
-    ["polygonMainnet", "mainnet.polygonscan.com"],
-    ["polygonMumbai", "mumbai.polygonscan.com"],
-])
+  ["ethMainnet", "mainnet.etherscan.io"],
+  ["ethGoerli", "goerli.etherscan.io"],
+  ["ethKovan", "kovan.etherscan.io"],
+  ["ethRinkeby", "rinkeby.etherscan.io"],
+  ["ethRopsten", "ropsten.etherscan.io"],
+  ["polygonMainnet", "mainnet.polygonscan.com"],
+  ["polygonMumbai", "mumbai.polygonscan.com"],
+]);
 
 export function logContractDeploymentInfo(networkName: string, contractName: string, contract: Contract) {
-    console.log(
-        `${contractName} is deployed at address: ${contract.address}\n`, 
-        `> Explorer URL for Contract: https://${NETWORK_TO_EXPLORER_URL.get(networkName)}/address/${contract.address}\n`,
-        contract.deployTransaction ? `> Explorer URL for Deployment Tx: https://${NETWORK_TO_EXPLORER_URL.get(networkName)}/tx/${contract.deployTransaction.hash}\n` : '',
-    )
+  console.log(
+    `${contractName} is deployed at address: ${contract.address}\n`,
+    `> Explorer URL for Contract: https://${NETWORK_TO_EXPLORER_URL.get(networkName)}/address/${contract.address}\n`,
+    contract.deployTransaction
+      ? `> Explorer URL for Deployment Tx: https://${NETWORK_TO_EXPLORER_URL.get(networkName)}/tx/${
+          contract.deployTransaction.hash
+        }\n`
+      : "",
+  );
 }
 
 export function getTxExplorerLink(networkName: string, tx: ContractTransaction) {
-    return `https://${NETWORK_TO_EXPLORER_URL.get(networkName)}/tx/${tx.hash}\n`
+  return `https://${NETWORK_TO_EXPLORER_URL.get(networkName)}/tx/${tx.hash}\n`;
 }
