b9232dd75371e6c9839e0e9fdbe3972e76333914
Nadim Abrar <nadimabrar@gmail.com>
2022-01-19 15:45:55 +0600

    Deploy smart contracts and upload addresses to S3 bucket during release

--- scripts/deploy-dummy.ts
+++ scripts/deploy-dummy.ts
@@ -1,6 +1,7 @@
 import { ethers } from "hardhat";
 import { config as dotenvConfig } from "dotenv";
 import { resolve } from "path";
+import { addToAddressBook } from "../utils/address-book"
 
 dotenvConfig({ path: resolve(__dirname, "./.env") });
 
@@ -16,6 +17,7 @@ async function deployDummy(): Promise<any> {
   for (const [price, [name, symbol]] of Object.entries(prices)) {
     const vault = await DummyFactory.deploy(name, symbol, price);
     await vault.deployed();
+    await addToAddressBook(name, vault.address)
     console.log(`vault at ${vault.address} deployed`);
   }
 }
---
+++ utils/address-book.ts
@@ -0,0 +1,15 @@
+import { resolve, join } from "path";
+import { readJSON, outputJSON } from "fs-extra";
+
+export async function addToAddressBook(contractName: string, address: string) {
+    const addressBookDir = resolve(__dirname, "..");
+    const addressBookPath = join(addressBookDir, 'addressbook.json');
+    let addressBook;
+    try {
+        addressBook = await readJSON(addressBookPath) 
+    } catch (err) {
+        addressBook = {}
+    }
+    addressBook[contractName] = address
+    await outputJSON(addressBookPath, addressBook, { spaces: 2 })
+}
