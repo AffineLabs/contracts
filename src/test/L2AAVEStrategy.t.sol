12154a06040c1f326d7e315d54f232c9b2633967
larryob <9146641+larryob@users.noreply.github.com>
2022-01-26 15:51:12 -0500

    Add foundry.toml

--- src/test/L2AAVEStrategy.t.sol
+++ src/test/L2AAVEStrategy.t.sol
@@ -11,7 +11,7 @@ import { IWormhole } from "../interfaces/IWormhole.sol";
 import { L2AAVEStrategy } from "../polygon-contracts/L2AAVEStrategy.sol";
 
 // TODO: make it so that the first test always works => Truncation means the assert will fail at some blocks
-contract AAVEStratTest is DSTest {
+contract AAVEStratTestFork is DSTest {
     L2Vault vault;
     Create2Deployer create2Deployer;
     L2AAVEStrategy strategy;
