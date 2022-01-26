ad5ea4962a2d594fdcc768454c425000992ca44f
larryob <9146641+larryob@users.noreply.github.com>
2022-01-26 02:29:21 -0500

    Add more strategy tests
    - Update profit test to include actually making a profit with `warp`
    
    - Add a test where strategy loses money
    
    - Use foundry for impersonating strategy

--- src/test/IHevm.sol
+++ src/test/IHevm.sol
@@ -7,4 +7,10 @@ interface IHevm {
         bytes32 loc,
         bytes32 val
     ) external;
+
+    function warp(uint256 x) external;
+
+    function startPrank(address sender) external;
+
+    function stopPrank() external;
 }
