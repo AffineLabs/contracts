4bdef1b8e885a479bea5f1c43a664168b489db58
Nadim Abrar <nadimabrar@gmail.com>
2022-02-09 13:55:14 +0600

    Move strings to enums to make codes less error prone

--- utils/constants/blockchain.ts
+++ utils/constants/blockchain.ts
@@ -1,25 +1,46 @@
 import { BlockchainInfo } from "./types";
 
+enum blockchains {
+    ETHEREUM = "Ethereum",
+    POYLGON = "Polygon",
+}
+
+enum ethNetworks {
+    MAINNET = "Mainnet",
+    GOERLI = "Goerli",
+}
+
+enum polygonNetworks {
+    MAINNET = "Mainnet",
+    MUMBAI = "Mumbai",
+}
+
+enum proofFormats {
+    POA = "POA",
+    POS = "POS",
+    POW = "POW",
+}
+
 export const ETH_GOERLI: BlockchainInfo = {
-  name: "Ethereum",
-  network: "Goerli",
-  proof_format: "POA",
+    name: blockchains.ETHEREUM,
+    network: ethNetworks.GOERLI,
+    proof_format: proofFormats.POA,
 } as const;
 
 export const ETH_MAINNET: BlockchainInfo = {
-  name: "Ethereum",
-  network: "Mainnet",
-  proof_format: "POW",
+    name: blockchains.ETHEREUM,
+    network: ethNetworks.MAINNET,
+    proof_format: proofFormats.POW,
 } as const;
 
 export const POLYGON_MUMBAI: BlockchainInfo = {
-  name: "Polygon",
-  network: "Goerli",
-  proof_format: "POS",
+    name: blockchains.POYLGON,
+    network: polygonNetworks.MUMBAI,
+    proof_format: proofFormats.POS,
 } as const;
 
 export const POLYGON_MAINNET: BlockchainInfo = {
-  name: "Polygon",
-  network: "Mainnet",
-  proof_format: "POS",
+    name: blockchains.POYLGON,
+    network: polygonNetworks.MAINNET,
+    proof_format: proofFormats.POS,
 } as const;
