d8d72f4352513ecf2a8b3bc07c3599fa8f78119c
Nadim Abrar <nadimabrar@gmail.com>
2022-02-23 16:04:15 +0600

    Update deploy all function

--- scripts/utils/deploy-all.ts
+++ scripts/utils/deploy-all.ts
@@ -1,7 +1,7 @@
 import { deployVaults, VaultContracts } from "./deploy-vaults";
 import { Config } from "../../utils/config";
 import { deployStrategies, StrategyContracts } from "./deploy-strategies";
-import { ethers } from "ethers";
+import { address } from "../../utils/types";
 
 export interface AllContracts {
   vaults: VaultContracts;
@@ -9,11 +9,19 @@ export interface AllContracts {
 }
 
 export async function deployAll(
+  l1Governance: address,
+  l2Governance: address,
   ethNetworkName: string,
   polygonNetworkName: string,
   config: Config,
 ): Promise<AllContracts> {
-  const vaults = await deployVaults(ethNetworkName, polygonNetworkName, config);
+  const vaults = await deployVaults(
+    l1Governance,
+    l2Governance,
+    ethNetworkName,
+    polygonNetworkName,
+    config,
+  );
   const strategies = await deployStrategies(ethNetworkName, polygonNetworkName, vaults);
 
   console.log("Adding strategies to vault...");
