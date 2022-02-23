b576f0fc2d85cd096616c62d5ed2f6a238646384
Nadim Abrar <nadimabrar@gmail.com>
2022-02-23 14:47:18 +0600

    Fix formatting

--- scripts/testnet/deploy-strategy.ts
+++ scripts/testnet/deploy-strategy.ts
@@ -9,11 +9,7 @@ async function deployAAVE(): Promise<any> {
   // Using address of USDC compatible with AAVE on mumbai
   const myConfig = { ...config };
   myConfig.l2USDC = "0x2058A9D7613eEE744279e3856Ef0eAda5FCbaA7e";
-  const vaultContracts = await scriptUtils.deployVaults(
-    "ethGoerli",
-    "polygonMumbai",
-    myConfig,
-  );
+  const vaultContracts = await scriptUtils.deployVaults("ethGoerli", "polygonMumbai", myConfig);
 
   const stratFactory = await ethers.getContractFactory("L2AAVEStrategy", deployer);
 
