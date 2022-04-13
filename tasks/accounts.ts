05f8c6c7195c8a70fd346ff1ed57a2641b64e957
larryob <tosin@tryalpine.com>
2022-04-13 17:17:20 -0400

    Don't compile forge's test directory

--- tasks/accounts.ts
+++ tasks/accounts.ts
@@ -1,4 +1,6 @@
 import { task } from "hardhat/config";
+import { subtask } from "hardhat/config";
+import { TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS } from "hardhat/builtin-tasks/task-names";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -8,4 +10,10 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
   for (const account of accounts) {
     console.log(await account.getAddress());
   }
+});
+
+subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS).setAction(async (_, __, runSuper) => {
+  const paths = await runSuper();
+  // Don't compile anything under src/test/
+  return paths.filter((p: string) => !p.includes("src/test/"));
 });
