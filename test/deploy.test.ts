5654fbdcbcaf6dfadc4127ffbdd7037f73cd1018
Nadim Abrar <nadimabrar@gmail.com>
2022-05-12 21:49:59 +0600

    Fix deploy test

--- test/deploy.test.ts
+++ test/deploy.test.ts
@@ -3,18 +3,24 @@ import chai from "chai";
 import { solidity } from "ethereum-waffle";
 
 import { deployVaults } from "../scripts/helpers/deploy-vaults";
+import { deployWormholeRouters } from "../scripts/helpers/deploy-wormhole-router";
 import { config } from "../scripts/utils/config";
 
 chai.use(solidity);
 const { expect } = chai;
 
 it("Deploy Vaults", async () => {
+  const wormholeRouters = await deployWormholeRouters(
+    process.env.ETH_NETWORK || "eth-goerli-fork",
+    process.env.POLYGON_NETWORK || "polygon-mumbai-fork",
+  );
   const { l1Vault, l2Vault } = await deployVaults(
     config.l1Governance,
     config.l2Governance,
     process.env.ETH_NETWORK || "eth-goerli-fork",
     process.env.POLYGON_NETWORK || "polygon-mumbai-fork",
     config,
+    wormholeRouters,
   );
 
   // If tokens are set correctly, most likely everything else is.
