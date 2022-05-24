c545dffc7d7a1b5098d8fa856da000445392ef08
Mahmoud Khalifa <mahmoudk@mit.edu>
2022-05-24 19:13:32 +0300

    BaseVault test change
    Added a strategy addition test

--- src/test/BaseVault.t.sol
+++ src/test/BaseVault.t.sol
@@ -9,6 +9,27 @@ import { MockERC20 } from "./MockERC20.sol";
 import { BaseStrategy } from "../BaseStrategy.sol";
 import { BaseVault } from "../BaseVault.sol";
 
+contract TestStrategy is BaseStrategy {
+    constructor(MockERC20 _token, BaseVault _vault) {
+        token = _token;
+        vault = _vault;
+    }
+
+    function balanceOfToken() public view override returns (uint256) {
+        return 0;
+    }
+
+    function invest(uint256 amount) public override {}
+
+    function divest(uint256 amount) public override returns (uint256) {
+        return 0;
+    }
+
+    function totalLockedValue() public override returns (uint256) {
+        return 0;
+    }
+}
+
 contract VaultTest is TestPlus {
     BaseVault vault;
     MockERC20 token;
@@ -20,5 +41,13 @@ contract VaultTest is TestPlus {
 
     function testHarvest() public {
         assertTrue(1 == 1);
+    }
+
+    function testStrategyAddition() public {
+        TestStrategy strategy = new TestStrategy(token, vault);
+        vault.addStrategy(strategy, 1000);
+        assertEq(address(vault.withdrawalQueue(0)), address(strategy));
+        (, uint256 tvlBps, , , ) = vault.strategies(strategy);
+        assertEq(tvlBps, 1000);
     }
 }
