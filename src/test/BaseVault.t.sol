87c308688f08534f37498b88849799233c49f89b
Mahmoud Khalifa <mahmoudk@mit.edu>
2022-05-31 22:22:28 +0300

    Added Withdrawal Queue tests

--- src/test/BaseVault.t.sol
+++ src/test/BaseVault.t.sol
@@ -33,6 +33,7 @@ contract TestStrategy is BaseStrategy {
 contract VaultTest is TestPlus {
     BaseVault vault;
     MockERC20 token;
+    uint8 constant MAX_STRATEGIES = 20;
 
     function setUp() public {
         vault = Deploy.deployBaseVault();
@@ -58,5 +59,44 @@ contract VaultTest is TestPlus {
         assertEq(tvlBps, 0);
         assertTrue(isActive == false);
         assertEq(address(vault.withdrawalQueue(0)), address(0));
+    }
+
+    function testGetWithdrawalQueue() public {
+        for (uint256 i = 0; i < vault.MAX_STRATEGIES(); i++) {
+            assertTrue(vault.getWithdrawalQueue()[i] == vault.withdrawalQueue(i));
+        }
+    }
+
+    event WithdrawalQueueSet(address indexed user, BaseStrategy[MAX_STRATEGIES] replacedWithdrawalQueue);
+
+    event WithdrawalQueueIndexesSwapped(
+        address indexed user,
+        uint256 index1,
+        uint256 index2,
+        BaseStrategy indexed newStrategy1,
+        BaseStrategy indexed newStrategy2
+    );
+
+    function testSetWithdrawalQueue() public {
+        BaseStrategy[MAX_STRATEGIES] memory newQueue;
+        for (uint256 i = 0; i < MAX_STRATEGIES; i++) {
+            newQueue[i] = new TestStrategy(token, vault);
+        }
+        vault.setWithdrawalQueue(newQueue);
+        for (uint256 i = 0; i < MAX_STRATEGIES; i++) {
+            assertTrue(vault.withdrawalQueue(i) == newQueue[i]);
+        }
+    }
+
+    function testSwapWithdrawalQueue() public {
+        vault.addStrategy(new TestStrategy(token, vault), 1000);
+        vault.addStrategy(new TestStrategy(token, vault), 2000);
+        BaseStrategy newStrategy1 = vault.withdrawalQueue(0);
+        BaseStrategy newStrategy2 = vault.withdrawalQueue(1);
+        vm.expectEmit(true, true, true, true);
+        emit WithdrawalQueueIndexesSwapped(address(this), 0, 1, newStrategy2, newStrategy1);
+        vault.swapWithdrawalQueueIndexes(0, 1);
+        assertTrue(newStrategy1 == vault.withdrawalQueue(1));
+        assertTrue(newStrategy2 == vault.withdrawalQueue(0));
     }
 }
