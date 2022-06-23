6537ba7d6c83416ff1766ba12e6b2efd7d707c43
Mahmoud Khalifa <mahmoudk@mit.edu>
2022-06-23 14:10:41 -0400

    Added deployment of router

--- scripts/helpers/deploy-all.ts
+++ scripts/helpers/deploy-all.ts
@@ -10,12 +10,15 @@ import { addToAddressBookAndDefender, getContractAddress } from "../utils/export
 import { POLYGON_MUMBAI } from "../utils/constants/blockchain";
 import { sign } from "crypto";
 import { deployWormholeRouters, WormholeRouterContracts } from "./deploy-wormhole-router";
+import { deployRouter } from "./deploy-router";
+import { Router } from "typechain/src/polygon";
 
 export interface AllContracts {
   wormholeRouters: WormholeRouterContracts;
   vaults: VaultContracts;
   strategies: StrategyContracts;
   basket: TwoAssetBasket;
+  router: Router;
 }
 
 export async function deployAll(
@@ -35,7 +38,7 @@ export async function deployAll(
     wormholeRouters,
   );
   const strategies = await deployStrategies(ethNetworkName, polygonNetworkName, vaults);
-
+  const router = await deployRouter();
   // TODO: Consider strategies. We can't add strategies anymore since the timelock address is the governance address
   // In tests we can simply use hardhat's mocking abilities.
 
@@ -84,5 +87,6 @@ export async function deployAll(
     vaults,
     strategies,
     basket,
+    router,
   };
 }
---
+++ scripts/helpers/deploy-router.ts
@@ -0,0 +1,13 @@
+import { ethers } from "hardhat";
+import { Router } from "../../typechain";
+import { POLYGON_MUMBAI } from "../utils/constants/blockchain";
+import { addToAddressBookAndDefender } from "../utils/export";
+
+export async function deployRouter(): Promise<Router> {
+  const [deployer] = await ethers.getSigners();
+  const RouterFactory = await ethers.getContractFactory("Router");
+  const router = await RouterFactory.deploy(); //Add more stuff?
+  await router.deployed();
+  await addToAddressBookAndDefender(POLYGON_MUMBAI, "ERC4626Router", "Router", router); //unsure what tag should be
+  return router;
+}
