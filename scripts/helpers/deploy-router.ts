63a4f43d334d4ba6d9fcfccf5dac400409c0dda3
Mahmoud Khalifa <mahmoudk@mit.edu>
2022-06-23 15:41:14 -0400

    Added new Router

--- scripts/helpers/deploy-router.ts
+++ scripts/helpers/deploy-router.ts
@@ -6,7 +6,7 @@ import { addToAddressBookAndDefender } from "../utils/export";
 export async function deployRouter(): Promise<Router> {
   const [deployer] = await ethers.getSigners();
   const RouterFactory = await ethers.getContractFactory("Router");
-  const router = await RouterFactory.deploy(); //Add more stuff?
+  const router = await RouterFactory.deploy(""); //Add more stuff?
   await router.deployed();
   await addToAddressBookAndDefender(POLYGON_MUMBAI, "ERC4626Router", "Router", router); //unsure what tag should be
   return router;
