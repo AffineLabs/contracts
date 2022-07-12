3042bcd8d4a9aee46ebd115156aec3a74a86fe2d
larryob <tosin@tryalpine.com>
2022-07-12 15:37:50 -0400

    Actually invest/divest in `MintableStrategy`

--- src/testnet/MintableStrategy.sol
+++ src/testnet/MintableStrategy.sol
@@ -2,12 +2,15 @@
 pragma solidity ^0.8.13;
 
 import { ERC20 } from "solmate/src/tokens/ERC20.sol";
+import { SafeTransferLib } from "solmate/src/utils/SafeTransferLib.sol";
 
 import { MintableToken } from "./MintableToken.sol";
 import { BaseStrategy } from "../BaseStrategy.sol";
 import { L2Vault } from "../polygon/L2Vault.sol";
 
 contract MintableStrategy is BaseStrategy {
+    using SafeTransferLib for ERC20;
+
     constructor(L2Vault _vault) {
         vault = _vault;
         asset = ERC20(vault.asset());
@@ -27,13 +30,13 @@ contract MintableStrategy is BaseStrategy {
         return asset.balanceOf(address(this));
     }
 
-    function invest(uint256 amount) external pure override {
-        amount;
+    function invest(uint256 amount) external override {
+        asset.safeTransferFrom(msg.sender, address(this), amount);
     }
 
-    function divest(uint256 amount) external pure override returns (uint256) {
-        amount;
-        return 0;
+    function divest(uint256 amount) external override returns (uint256) {
+        asset.transfer(address(vault), amount);
+        return amount;
     }
 
     function totalLockedValue() public view override returns (uint256) {
