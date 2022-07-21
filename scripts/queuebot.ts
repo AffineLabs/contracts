f83adad791baf5586226950561861094573c12f8
Nadimul Abrar <nadimabrar@gmail.comr>
2022-07-21 17:54:37 +0600

    Fix format

--- scripts/queuebot.ts
+++ scripts/queuebot.ts
@@ -10,29 +10,30 @@ const l2VaultABI = JSON.parse(readFileSync(`${abiDir}/L2Vault.json`).toString())
 const emergencyWithdrawalQueueABI = JSON.parse(readFileSync(`${abiDir}/EmergencyWithdrawalQueue.json`).toString());
 
 async function main() {
-  const { 
-    mnemonic,
-    polygonAlchemyURL,
-  } = REBALANCE_CONFIG;
+  const { mnemonic, polygonAlchemyURL } = REBALANCE_CONFIG;
 
   const mumbaiProvider = new ethers.providers.JsonRpcProvider(polygonAlchemyURL);
 
   const polygonWallet = Wallet.fromMnemonic(mnemonic).connect(mumbaiProvider);
 
-  const addrBook  = await readAddressBook();
+  const addrBook = await readAddressBook();
   const l2Vault = new Contract(addrBook.EthAlpSave.address, l2VaultABI, polygonWallet);
-  const emergencyWithdrawalQueue = new Contract(await l2Vault.emergencyWithdrawalQueue(), emergencyWithdrawalQueueABI, polygonWallet);
+  const emergencyWithdrawalQueue = new Contract(
+    await l2Vault.emergencyWithdrawalQueue(),
+    emergencyWithdrawalQueueABI,
+    polygonWallet,
+  );
 
   while (true) {
     const curQueueSize = await emergencyWithdrawalQueue.size();
     if (!curQueueSize) {
-        break;
+      break;
     }
     try {
-        await emergencyWithdrawalQueue.dequeue(Math.min(curQueueSize, 100));
+      await emergencyWithdrawalQueue.dequeue(Math.min(curQueueSize, 100));
     } catch (e) {
-        console.log(`Dequeue bot failed with:`, e);
-        break;
+      console.log(`Dequeue bot failed with:`, e);
+      break;
     }
   }
 }
