2f6e59c93a1f0da8eb5f587c6ea5c546ac6ef243
Nadimul Abrar <nadimabrar@gmail.comr>
2022-07-21 17:53:00 +0600

    Add queuebot script

---
+++ scripts/queuebot.ts
@@ -0,0 +1,48 @@
+import { Contract, ethers, Wallet } from "ethers";
+import { resolve } from "path";
+import { readFileSync } from "fs";
+import { readAddressBook } from "./utils/export";
+
+import { REBALANCE_CONFIG } from "./utils/config";
+
+const abiDir = resolve(__dirname, "../abi");
+const l2VaultABI = JSON.parse(readFileSync(`${abiDir}/L2Vault.json`).toString());
+const emergencyWithdrawalQueueABI = JSON.parse(readFileSync(`${abiDir}/EmergencyWithdrawalQueue.json`).toString());
+
+async function main() {
+  const { 
+    mnemonic,
+    polygonAlchemyURL,
+  } = REBALANCE_CONFIG;
+
+  const mumbaiProvider = new ethers.providers.JsonRpcProvider(polygonAlchemyURL);
+
+  const polygonWallet = Wallet.fromMnemonic(mnemonic).connect(mumbaiProvider);
+
+  const addrBook  = await readAddressBook();
+  const l2Vault = new Contract(addrBook.EthAlpSave.address, l2VaultABI, polygonWallet);
+  const emergencyWithdrawalQueue = new Contract(await l2Vault.emergencyWithdrawalQueue(), emergencyWithdrawalQueueABI, polygonWallet);
+
+  while (true) {
+    const curQueueSize = await emergencyWithdrawalQueue.size();
+    if (!curQueueSize) {
+        break;
+    }
+    try {
+        await emergencyWithdrawalQueue.dequeue(Math.min(curQueueSize, 100));
+    } catch (e) {
+        console.log(`Dequeue bot failed with:`, e);
+        break;
+    }
+  }
+}
+
+main()
+  .then(() => {
+    console.log("Dequeue session done!");
+    process.exit(0);
+  })
+  .catch((error: Error) => {
+    console.error(error);
+    process.exit(1);
+  });
