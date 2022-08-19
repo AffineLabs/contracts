6f2f3d79d5f3562bae2b49f973e039e69f96fbbf
larryob <tosin@tryalpine.com>
2022-08-19 17:01:17 -0400

    Add more L2Vault tests (#181)

---
+++ src/test/mocks/index.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.13;
+
+import { L2Vault } from "../../polygon/L2Vault.sol";
+
+// Mocks needed to update variables that are in packed slots (forge-std cannot write to packed slots yet)
+contract MockL2Vault is L2Vault {
+    function setCanTransferToL1(bool _can) external {
+        canTransferToL1 = _can;
+    }
+
+    function setCanRequestFromL1(bool _can) external {
+        canRequestFromL1 = _can;
+    }
+}
