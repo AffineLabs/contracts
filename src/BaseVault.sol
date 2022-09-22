471476f7b61f8f0851ad594c00fd027b10002368
larryob <tosin@tryalpine.com>
2022-09-22 16:15:58 -0400

    Use uncheckedInc

--- src/BaseVault.sol
+++ src/BaseVault.sol
@@ -13,6 +13,7 @@ import {AffineGovernable} from "./AffineGovernable.sol";
 import {BridgeEscrow} from "./BridgeEscrow.sol";
 import {WormholeRouter} from "./WormholeRouter.sol";
 import {Multicall} from "./external/Multicall.sol";
+import {uncheckedInc} from "./Unchecked.sol";
 
 /**
  * @notice A core contract to be inherited by the L1 and L2 vault contracts. This contract handles adding
@@ -206,8 +207,7 @@ abstract contract BaseVault is Initializable, AccessControl, AffineGovernable, M
         // number or empty values we've seen iterating from left to right
         uint256 offset;
 
-        uint256 length = withdrawalQueue.length;
-        for (uint256 i = 0; i < length; ++i) {
+        for (uint256 i = 0; i < MAX_STRATEGIES; i = uncheckedInc(i)) {
             Strategy strategy = withdrawalQueue[i];
             if (address(strategy) == address(0)) {
                 offset += 1;
@@ -226,7 +226,7 @@ abstract contract BaseVault is Initializable, AccessControl, AffineGovernable, M
      * and allows us to add any realized profits to our lockedProfit
      */
     function removeStrategy(Strategy strategy) external onlyGovernance {
-        for (uint256 i = 0; i < MAX_STRATEGIES; ++i) {
+        for (uint256 i = 0; i < MAX_STRATEGIES; i = uncheckedInc(i)) {
             if (strategy != withdrawalQueue[i]) continue;
 
             StrategyInfo storage stratInfo = strategies[strategy];
@@ -267,7 +267,7 @@ abstract contract BaseVault is Initializable, AccessControl, AffineGovernable, M
         external
         onlyRole(harvesterRole)
     {
-        for (uint256 i = 0; i < strategyList.length; ++i) {
+        for (uint256 i = 0; i < strategyList.length; i = uncheckedInc(i)) {
             // Get the strategy at the current index.
             Strategy strategy = strategyList[i];
 
@@ -307,7 +307,7 @@ abstract contract BaseVault is Initializable, AccessControl, AffineGovernable, M
     function _depositIntoStrategies() internal {
         uint256 totalBal = _asset.balanceOf(address(this));
         // All non-zero strategies are active
-        for (uint256 i = 0; i < MAX_STRATEGIES; ++i) {
+        for (uint256 i = 0; i < MAX_STRATEGIES; i = uncheckedInc(i)) {
             Strategy strategy = withdrawalQueue[i];
             if (address(strategy) == address(0)) {
                 break;
@@ -398,7 +398,7 @@ abstract contract BaseVault is Initializable, AccessControl, AffineGovernable, M
         uint256 totalProfitAccrued;
 
         // Will revert if any of the specified strategies are untrusted.
-        for (uint256 i = 0; i < strategyList.length; ++i) {
+        for (uint256 i = 0; i < strategyList.length; i = uncheckedInc(i)) {
             // Get the strategy at the current index.
             Strategy strategy = strategyList[i];
 
@@ -475,7 +475,7 @@ abstract contract BaseVault is Initializable, AccessControl, AffineGovernable, M
     function _liquidate(uint256 amount) internal returns (uint256) {
         uint256 amountLiquidated;
         uint256 length = withdrawalQueue.length;
-        for (uint256 i = 0; i < length; ++i) {
+        for (uint256 i = 0; i < length; i = uncheckedInc(i)) {
             Strategy strategy = withdrawalQueue[i];
             if (address(strategy) == address(0)) {
                 break;
@@ -513,7 +513,7 @@ abstract contract BaseVault is Initializable, AccessControl, AffineGovernable, M
         // MAX_STRATEGIES is always equal to withdrawalQueue.length
         uint256[MAX_STRATEGIES] memory amountsToInvest;
 
-        for (uint256 i = 0; i < MAX_STRATEGIES; ++i) {
+        for (uint256 i = 0; i < MAX_STRATEGIES; i = uncheckedInc(i)) {
             Strategy strategy = withdrawalQueue[i];
             if (address(strategy) == address(0)) {
                 break;
@@ -530,7 +530,7 @@ abstract contract BaseVault is Initializable, AccessControl, AffineGovernable, M
         }
 
         // Loop through the strategies to invest in, and invest in them
-        for (uint256 i = 0; i < MAX_STRATEGIES; ++i) {
+        for (uint256 i = 0; i < MAX_STRATEGIES; i = uncheckedInc(i)) {
             uint256 amountToInvest = amountsToInvest[i];
             if (amountToInvest == 0) continue;
 
