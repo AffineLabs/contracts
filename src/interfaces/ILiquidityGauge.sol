893c7202a2248e888d2a97920f9cbe119d362f08
larryob <tosin@tryalpine.com>
2022-09-23 16:23:41 -0400

    Fix lints

--- src/interfaces/ILiquidityGauge.sol
+++ src/interfaces/ILiquidityGauge.sol
@@ -2,7 +2,10 @@
 pragma solidity ^0.8.16;
 
 // See https://etherscan.io/address/0xd8b712d29381748dB89c36BCa0138d7c75866ddF#code
+/*  solhint-disable func-name-mixedcase, var-name-mixedcase */
 interface ILiquidityGauge {
     function deposit(uint256 _value) external;
     function claimable_tokens(address addr) external returns (uint256);
 }
+
+/*  solhint-disable func-name-mixedcase, var-name-mixedcase */
