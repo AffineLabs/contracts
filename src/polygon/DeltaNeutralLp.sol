24d682864eb705f74021c98b08235af8a60b8ff6
larryob <tosin@tryalpine.com>
2022-09-29 01:05:48 -0400

    Add SOSLP strategy
    Closes ALP-2586

---
+++ src/polygon/DeltaNeutralLp.sol
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.16;
+
+import {ERC20} from "solmate/src/tokens/ERC20.sol";
+import {SafeTransferLib} from "solmate/src/utils/SafeTransferLib.sol";
+import {FixedPointMathLib} from "solmate/src/utils/FixedPointMathLib.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+import {ILendingPoolAddressesProvider} from "../interfaces/aave/ILendingPoolAddressesProvider.sol";
+import {ILendingPool} from "../interfaces/aave/ILendingPool.sol";
+import {ILendingPool} from "../interfaces/aave/ILendingPool.sol";
+import {IUniLikeSwapRouter} from "../interfaces/IUniLikeSwapRouter.sol";
+import {AggregatorV3Interface} from "../interfaces/AggregatorV3Interface.sol";
+
+import {BaseVault} from "../BaseVault.sol";
+import {BaseStrategy} from "../BaseStrategy.sol";
+
+interface ILendingPoolAddressesProviderRegistry {
+    function getAddressesProvidersList() external view returns (address[] memory);
+}
+
+contract DeltaNeutralLp is BaseStrategy, Ownable {
+    using SafeTransferLib for ERC20;
+    using FixedPointMathLib for uint256;
+
+    constructor(
+        BaseVault _vault,
+        uint256 _slippageTolerance,
+        uint256 _longPct,
+        ILendingPoolAddressesProviderRegistry _registry,
+        ERC20 _borrowAsset,
+        AggregatorV3Interface _borrowAssetFeed,
+        IUniLikeSwapRouter _router
+    ) BaseStrategy(_vault) {
+        canStartNewPos = true;
+        slippageTolerance = _slippageTolerance;
+        longPercentage = _longPct;
+
+        address[] memory providers = _registry.getAddressesProvidersList();
+        address pool = ILendingPoolAddressesProvider(providers[providers.length - 1]).getLendingPool();
+        lendingPool = ILendingPool(pool);
+
+        borrowAssetFeed = _borrowAssetFeed;
+        borrowAsset = _borrowAsset;
+
+        router = _router;
+        asset.safeApprove(address(_router), type(uint256).max);
+        borrowAsset.safeApprove(address(_router), type(uint256).max);
+    }
+
+    function balanceOfAsset() public view override returns (uint256) {
+        return asset.balanceOf(address(this));
+    }
+
+    function totalLockedValue() public view override returns (uint256) {}
+
+    function invest(uint256 amount) external override {
+        asset.safeTransferFrom(msg.sender, address(this), amount);
+    }
+
+    function divest(uint256 amount) external override onlyVault returns (uint256) {}
+
+    uint32 public currentPosition;
+    bool public canStartNewPos;
+    mapping(uint256 => uint256) public getPositionTime;
+
+    event PositionStart(uint32 indexed position, uint256 timestamp);
+
+    uint256 public slippageTolerance;
+    /// @notice Fixed point number describing the percentage of the position with which to go long. 1e18 = 1 = 100%
+    uint256 public longPercentage;
+    IUniLikeSwapRouter public immutable router;
+    ERC20 public immutable borrowAsset;
+    ILendingPool immutable lendingPool;
+    /// @notice Gives ratio of vault asset to borrow asset, e.g. WMATIC/USD (assuming usdc = usd)
+    AggregatorV3Interface immutable borrowAssetFeed;
+
+    function startPosition() external onlyOwner {
+        // Set position metadata
+        require(canStartNewPos, "DNLP: position is active");
+        uint32 newPositionId = currentPosition + 1;
+        currentPosition = newPositionId;
+        getPositionTime[newPositionId] = block.timestamp;
+        canStartNewPos = false;
+        emit PositionStart(newPositionId, block.timestamp);
+
+        // Buy Matic (how much???)
+        uint256 assets = asset.balanceOf(address(this));
+        uint256 assetsToMatic = assets.mulWadDown(longPercentage);
+        address[] memory path = new address[](2);
+        path[0] = address(asset);
+        path[1] = address(borrowAsset);
+
+        router.swapExactTokensForTokens(assetsToMatic, 0, path, address(this), block.timestamp);
+
+        // Borrow Matic at 75% (88% liquidation threshold and 85.5% max LTV)
+        // If x is amount we want to deposit into aave
+        // .75x = Total - x => 1.75x = Total => x = Total / 1.75 => Total * 4/7
+        // Deposit  asset in aave
+        uint256 assetsToDeposit = (assets - assetsToMatic).mulDivDown(4, 7);
+        lendingPool.deposit(address(asset), assetsToDeposit, address(this), 0);
+
+        // Convert assetsToDeposit into `borrowAsset` (e.g. WMATIC) units
+        (uint80 roundId, int256 price,, uint256 timestamp, uint80 answeredInRound) = borrowAssetFeed.latestRoundData();
+        require(price > 0, "Chainlink price <= 0");
+        require(answeredInRound >= roundId, "Chainlink stale data");
+        require(timestamp != 0, "Chainlink round not complete");
+
+        // https://docs.aave.com/developers/v/2.0/the-core-protocol/lendingpool#borrow
+        // assetsToDeposit has price uints `asset`, price has units `asset / borrowAsset` ratio. so we divide by price
+        // Scaling price down since `asset` only has six decimals: https://docs.chain.link/docs/data-feeds/price-feeds/
+        // TODO: handle both cases (assetDecimals > priceDecimals as well)
+        // Also adding an extra 12 decimals to get to 18 decimals that WMATIC has
+        uint256 borrowAssetsDeposited = (assetsToDeposit / (uint256(price) / 1e2)) * 1e12;
+
+        lendingPool.borrow(address(borrowAsset), (borrowAssetsDeposited).mulDivDown(3, 4), 2, 0, msg.sender);
+
+        // Provide liquidity on uniswap
+        uint256 bBal = borrowAsset.balanceOf(address(this));
+        uint256 aBal = assets - assetsToMatic - assetsToDeposit;
+        router.addLiquidity(
+            address(asset),
+            address(borrowAsset),
+            aBal,
+            bBal,
+            aBal.mulDivDown(98, 100),
+            bBal.mulDivDown(98, 100),
+            address(this),
+            block.timestamp
+        );
+    }
+
+    function endPosition() external onlyOwner {}
+}
