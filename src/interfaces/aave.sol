d455a0b0d3a48c09081123ecf96a78437178d21d
larryob <tosin@tryalpine.com>
2022-10-03 19:22:42 -0400

    Update imports

--- src/interfaces/aave.sol
+++ src/interfaces/aave.sol
@@ -6,3 +6,4 @@ import {IAaveIncentivesController} from "./aave/IAaveIncentivesController.sol";
 import {ILendingPool} from "./aave/ILendingPool.sol";
 import {IAToken} from "./aave/IAToken.sol";
 import {ILendingPoolAddressesProviderRegistry} from "./aave/ILendingPoolAddressesProviderRegistry.sol";
+import {IProtocolDataProvider} from "./aave/IProtocolDataProvider.sol";
