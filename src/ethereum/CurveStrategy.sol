f228d1f8595c1cf727a6850e54a7cc4b9dfbb61d
larryob <tosin@tryalpine.com>
2022-10-07 03:30:49 -0400

    Add approvals and slippage protection

--- src/ethereum/CurveStrategy.sol
+++ src/ethereum/CurveStrategy.sol
@@ -22,6 +22,7 @@ contract CurveStrategy is BaseStrategy, Ownable {
 
     IUniswapV2Router02 public immutable router;
     ERC20 public immutable crv;
+    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     constructor(
         BaseVault _vault,
@@ -42,20 +43,21 @@ contract CurveStrategy is BaseStrategy, Ownable {
         asset.safeApprove(address(zapper), type(uint256).max);
         metaPool.safeApprove(address(zapper), type(uint256).max);
         metaPool.safeApprove(address(gauge), type(uint256).max);
+
+        // For trading crv
+        crv.safeApprove(address(router), type(uint256).max);
     }
 
     function invest(uint256 amount) external override {
         asset.safeTransferFrom(msg.sender, address(this), amount);
-        _deposit(amount);
     }
 
-    function _deposit(uint256 assets) internal {
+    function deposit(uint256 assets, uint256 minLpTokens) external onlyOwner {
         // e.g. in a MIM-3CRV metapool, the 0 index is for MIM and the next three are for the underlying
         // coins of 3CRV
         // In this particular metapool, the 1st, 2nd, and 3rd indices are for DAI, USDC, and USDT
         uint256[4] memory depositAmounts = [0, 0, assets, 0];
-        // Infinite slippage is probably bad
-        zapper.add_liquidity(address(metaPool), depositAmounts, 0);
+        zapper.add_liquidity(address(metaPool), depositAmounts, minLpTokens);
     }
 
     function depositInGauge() external onlyOwner {
@@ -63,7 +65,7 @@ contract CurveStrategy is BaseStrategy, Ownable {
     }
 
     function divest(uint256 assets) external override onlyVault returns (uint256) {
-        _withdraw(assets);
+        _withdraw(assets, 0, 0);
 
         uint256 amountToSend = Math.min(asset.balanceOf(address(this)), assets);
         asset.safeTransfer(address(vault), amountToSend);
@@ -75,26 +77,33 @@ contract CurveStrategy is BaseStrategy, Ownable {
      * @dev Useful in the case that we want to do multiple withdrawals ahead of a big divestment from the vault. Doing the
      * withdrawals manually (in chunks) will give us less slippage
      */
-    function withdrawAssets(uint256 assets) external onlyOwner {
-        _withdraw(assets);
+    function withdrawAssets(uint256 assets, uint256 minAssetsFromCrv, uint256 minAssetsFromLp) external onlyOwner {
+        _withdraw(assets, minAssetsFromCrv, minAssetsFromLp);
     }
 
     /// @notice Try to increase balance of `asset` to `assets`.
-    function _withdraw(uint256 assets) internal {
+    function _withdraw(uint256 assets, uint256 minAssetsFromCrv, uint256 minAssetsFromLp) internal {
         uint256 currAssets = asset.balanceOf(address(this));
         if (currAssets >= assets) {
             return;
         }
 
         // Sell rewards if we have at least 10 CRV tokens
-        uint256 crvBal = gauge.claimable_tokens(address(this));
+        gauge.claim_rewards();
+        uint256 crvBal = crv.balanceOf(address(this));
 
-        address[] memory path = new address[](2);
-        path[0] = address(crv);
-        path[1] = address(asset);
+        address[] memory crvPath = new address[](3);
+        crvPath[0] = address(crv);
+        crvPath[1] = WETH;
+        crvPath[2] = address(asset);
 
         if (crvBal > 10e18) {
-            router.swapExactTokensForTokens(crvBal, 0, path, address(this), block.timestamp);
+            router.swapExactTokensForTokens(crvBal, minAssetsFromCrv, crvPath, address(this), block.timestamp);
+        }
+
+        currAssets = asset.balanceOf(address(this));
+        if (currAssets >= assets) {
+            return;
         }
 
         // Only divest the amount that you have to
@@ -102,11 +111,13 @@ contract CurveStrategy is BaseStrategy, Ownable {
         uint256[4] memory withdrawAmounts = [0, 0, assetsToDivest, 0];
 
         // If the amount  of lp tokens is greater than we have, simply burn the max amount of tokens
-        try zapper.remove_liquidity_imbalance(address(metaPool), withdrawAmounts, metaPool.balanceOf(address(this))) {
+        try zapper.remove_liquidity_imbalance(address(metaPool), withdrawAmounts, type(uint256).max) {
             // We successfully withdrew `assetsToDivest` and our balance is now `assets`
         } catch (bytes memory) {
             // We didn't have enough lp tokens to withdraw `assetsToDivest`. So we just burn all of our lp shares
-            zapper.remove_liquidity_one_coin(address(metaPool), metaPool.balanceOf(address(this)), assetIndex, 0);
+            zapper.remove_liquidity_one_coin(
+                address(metaPool), metaPool.balanceOf(address(this)), assetIndex, minAssetsFromLp
+            );
         }
     }
 
@@ -116,11 +127,12 @@ contract CurveStrategy is BaseStrategy, Ownable {
 
         uint256 assetsFromCrv;
         if (crvBal > 0) {
-            address[] memory path = new address[](2);
-            path[0] = address(crv);
-            path[1] = address(asset);
+            address[] memory crvPath = new address[](3);
+            crvPath[0] = address(crv);
+            crvPath[1] = WETH;
+            crvPath[2] = address(asset);
 
-            uint256[] memory amounts = router.getAmountsOut(crvBal, path);
+            uint256[] memory amounts = router.getAmountsOut(crvBal, crvPath);
             assetsFromCrv = amounts[amounts.length - 1];
         }
 
--- src/interfaces/curve/ILiquidityGauge.sol
+++ src/interfaces/curve/ILiquidityGauge.sol
@@ -6,6 +6,7 @@ pragma solidity ^0.8.16;
 interface ILiquidityGauge {
     function deposit(uint256 _value) external;
     function claimable_tokens(address addr) external returns (uint256);
+    function claim_rewards() external;
 }
 
 /*  solhint-disable func-name-mixedcase, var-name-mixedcase */
