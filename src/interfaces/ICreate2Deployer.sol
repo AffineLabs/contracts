1f298d6600c90d9d92637bd413b8922504b9d4a5
larryob <tosin@tryalpine.com>
2022-10-17 01:24:28 -0400

    rm Create2Deployer interface

--- src/interfaces/ICreate2Deployer.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity =0.8.16;
-
-interface ICreate2Deployer {
-    function deploy(uint256 value, bytes32 salt, bytes memory code) external returns (address);
-
-    function computeAddress(bytes32 salt, bytes32 codeHash) external view returns (address);
-}
