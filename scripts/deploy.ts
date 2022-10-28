9318af2d4fc8b1ef9773717a3f26c874ad12330d
larryob <tosin@tryalpine.com>
2022-10-27 20:50:40 -0400

    Add --layer flag

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,36 +6,39 @@ import { resolve } from "path";
 dotenvConfig({ path: resolve(__dirname, "../.env") });
 import { defenderEth, defenderPolygon } from "./utils/export";
 
-function executeScript(options: { layer1: boolean; mainnet: boolean; broadcast: boolean }) {
-  const ALCHEMY_ETH_KEY =
-    (options.mainnet ? process.env.ALCHEMY_ETH_MAINNET_KEY : process.env.ALCHEMY_ETH_GOERLI_KEY) || "";
+function executeScript(options: { layer: "1" | "2"; testnet: boolean; broadcast: boolean }) {
+  const isTest = options.testnet;
+  const isLayer1 = options.layer === "1";
+  if (options.layer !== "1" && options.layer !== "2") throw Error("Invalid layer");
+
+  const ALCHEMY_ETH_KEY = (isTest ? process.env.ALCHEMY_ETH_GOERLI_KEY : process.env.ALCHEMY_ETH_MAINNET_KEY) || "";
   const ALCHEMY_POLYGON_KEY =
-    (options.mainnet ? process.env.ALCHEMY_POLYGON_MAINNET_KEY : process.env.ALCHEMY_POLYGON_MUMBAI_KEY) || "";
+    (isTest ? process.env.ALCHEMY_POLYGON_MUMBAI_KEY : process.env.ALCHEMY_POLYGON_MAINNET_KEY) || "";
 
-  const ethNet = options.mainnet ? "mainnet" : "goerli";
-  const polygonNet = options.mainnet ? "mainnet" : "mumbai";
+  const ethNet = isTest ? "goerli" : "mainnet";
+  const polygonNet = isTest ? "mumbai" : "mainnet";
 
-  const rpcUrl = options.layer1
+  const rpcUrl = isLayer1
     ? `https://eth-${ethNet}.alchemyapi.io/v2/${ALCHEMY_ETH_KEY}`
     : `https://polygon-${polygonNet}.g.alchemy.com/v2/${ALCHEMY_POLYGON_KEY}`;
 
-  const contract = options.layer1 ? "L1.s.sol:Deploy" : "L2.s.sol:Deploy";
+  const contract = isLayer1 ? "L1.s.sol:Deploy" : "L2.s.sol:Deploy";
 
   const broadcast = options.broadcast ? "--broadcast" : "";
 
   const scriptCommand = `forge script script/${contract} --rpc-url ${rpcUrl} --ffi -vvv ${broadcast}`;
   execSync(scriptCommand, { stdio: "inherit" });
-  if (options.layer1) defenderEth(!options.broadcast, !options.mainnet);
-  else defenderPolygon(!options.broadcast, !options.mainnet);
+  if (isLayer1) defenderEth(!options.broadcast, isTest);
+  else defenderPolygon(!options.broadcast, isTest);
 }
 
 program
-  .option("-l1, --layer1", "Run script on ethereum, otherwise run on polygon")
-  .option("-m, --mainnet", "Run script on mainnet version of eth/polygon. Otherwise use goerli/mumbai")
+  .requiredOption("-l, --layer <layer>", "The layer to run the script on. Use 1 for eth and 2 for polygon")
+  .option("-t, --testnet", "Run script on goerli/mumbai instead of eth/polygon")
   .option("-b, --broadcast", "Actually broadcast transactions");
 
 program.parse();
 
-const options = program.opts() as { layer1: boolean; mainnet: boolean; broadcast: boolean };
+const options = program.opts() as { layer: "1" | "2"; testnet: boolean; broadcast: boolean };
 console.log({ options });
 executeScript(options);
