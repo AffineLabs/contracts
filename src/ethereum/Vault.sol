5ac3a6dc24041368ba59f22406d8304155b959de
larryob <tosin@tryalpine.com>
2022-12-01 18:53:34 -0500

    Add Affine4626

--- src/ethereum/Vault.sol
+++ src/ethereum/Vault.sol
@@ -10,8 +10,9 @@ import {ERC20} from "solmate/src/tokens/ERC20.sol";
 
 import {BaseVault} from "../BaseVault.sol";
 import {BridgeEscrow} from "../BridgeEscrow.sol";
+import {Affine4626} from "../Affine4626.sol";
 
-contract Vault is BaseVault, ERC4626Upgradeable {
+contract Vault is BaseVault, Affine4626 {
     using MathUpgradeable for uint256;
 
     function initalize(address _governance, address vaultAsset) external virtual initializer {
@@ -26,73 +27,5 @@ contract Vault is BaseVault, ERC4626Upgradeable {
     /// @notice See {IERC4626-totalAssets}
     function totalAssets() public view virtual override returns (uint256) {
         return vaultTVL() - lockedProfit();
-    }
-
-    function maxDeposit(address) public view virtual override returns (uint256) {
-        return type(uint256).max;
-    }
-
-    /**
-     * @dev See {IERC4262-deposit}.
-     */
-    function deposit(uint256 assets, address receiver) public virtual override returns (uint256) {
-        uint256 shares = previewDeposit(assets);
-        _deposit(_msgSender(), receiver, assets, shares);
-
-        return shares;
-    }
-
-    /**
-     * @dev See {IERC4262-mint}.
-     */
-    function mint(uint256 shares, address receiver) public virtual override returns (uint256) {
-        uint256 assets = previewMint(shares);
-        _deposit(_msgSender(), receiver, assets, shares);
-
-        return assets;
-    }
-
-    /**
-     * @dev See {IERC4262-withdraw}.
-     */
-    function withdraw(uint256 assets, address receiver, address owner) public virtual override returns (uint256) {
-        uint256 shares = previewWithdraw(assets);
-        _withdraw(_msgSender(), receiver, owner, assets, shares);
-
-        return shares;
-    }
-
-    /**
-     * @dev See {IERC4262-redeem}.
-     */
-    function redeem(uint256 shares, address receiver, address owner) public virtual override returns (uint256) {
-        uint256 assets = previewRedeem(shares);
-        _withdraw(_msgSender(), receiver, owner, assets, shares);
-
-        return assets;
-    }
-
-    function _convertToShares(uint256 assets, MathUpgradeable.Rounding rounding)
-        internal
-        view
-        virtual
-        override
-        returns (uint256 shares)
-    {
-        uint256 _totalSupply = totalSupply() + 1e8;
-        uint256 _totalAssets = totalAssets() + 1;
-        return assets.mulDiv(_totalSupply, _totalAssets, rounding);
-    }
-
-    function _convertToAssets(uint256 shares, MathUpgradeable.Rounding rounding)
-        internal
-        view
-        virtual
-        override
-        returns (uint256 assets)
-    {
-        uint256 _totalSupply = totalSupply() + 1e8;
-        uint256 _totalAssets = totalAssets() + 1;
-        return shares.mulDiv(_totalAssets, _totalSupply, rounding);
     }
 }
