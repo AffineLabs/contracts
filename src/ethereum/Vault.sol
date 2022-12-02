67d8fc26abe4d325f5ab10976c0ca1739de17e6e
larryob <tosin@tryalpine.com>
2022-12-01 19:18:18 -0500

    Add AffineVault

--- src/ethereum/Vault.sol
+++ src/ethereum/Vault.sol
@@ -2,26 +2,22 @@
 pragma solidity =0.8.16;
 
 import {ERC4626Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
-import {MathUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/math/MathUpgradeable.sol";
 import {IERC20MetadataUpgradeable} from
     "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/IERC20MetadataUpgradeable.sol";
 
 import {ERC20} from "solmate/src/tokens/ERC20.sol";
 
-import {BaseVault} from "../BaseVault.sol";
-import {BridgeEscrow} from "../BridgeEscrow.sol";
+import {AffineVault} from "../AffineVault.sol";
 import {Affine4626} from "../Affine4626.sol";
 
-contract Vault is BaseVault, Affine4626 {
-    using MathUpgradeable for uint256;
-
+contract Vault is AffineVault, Affine4626 {
     function initalize(address _governance, address vaultAsset) external virtual initializer {
-        BaseVault.baseInitialize(_governance, ERC20(vaultAsset), address(0), BridgeEscrow(address(0)));
+        AffineVault.baseInitialize(_governance, ERC20(vaultAsset));
         ERC4626Upgradeable.__ERC4626_init(IERC20MetadataUpgradeable(vaultAsset));
     }
 
-    function asset() public view override (BaseVault, ERC4626Upgradeable) returns (address) {
-        return BaseVault.asset();
+    function asset() public view override (AffineVault, ERC4626Upgradeable) returns (address) {
+        return AffineVault.asset();
     }
 
     /// @notice See {IERC4626-totalAssets}
