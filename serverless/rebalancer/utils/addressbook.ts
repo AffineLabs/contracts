583b919c8a539c94e54b66d8a0cfc888942652bf
Nadimul Abrar <nadimabrar@gmail.com>
2022-12-05 19:06:33 +0600

    Move rebalancer to bots repo

--- serverless/rebalancer/utils/addressbook.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export async function readAddressBook(contractVersion: string = "stable") {
-  const { data: addressBook } = await axios.get(
-    `https://sc-abis.s3.us-east-2.amazonaws.com/${contractVersion}/addressbook.json`,
-  );
-  return addressBook;
-}
