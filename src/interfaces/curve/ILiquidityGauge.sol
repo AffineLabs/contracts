9b5c0c3841f1601143e6e1d0dc6cf9831ad51c1d
larryob <tosin@tryalpine.com>
2022-12-29 18:47:00 -0500

    Update gauge ABI

--- src/interfaces/curve/ILiquidityGauge.sol
+++ src/interfaces/curve/ILiquidityGauge.sol
@@ -6,9 +6,18 @@ pragma solidity =0.8.16;
 interface ILiquidityGauge {
     function deposit(uint256 _value) external;
     function withdraw(uint256 _value) external returns (uint256);
+    function balanceOf(address owner) external returns (uint256);
+    
+    // Get claimable CRV
     function claimable_tokens(address addr) external returns (uint256);
     function claim_rewards() external;
-    function balanceOf(address owner) external returns (uint256);
+  
+    // NOTE: The below only applies to non-CRV rewards
+    // claimable_reward - claimed_reward = pending rewards
+    // Total rewards, claimed and unclaimed
+    function claimable_reward(address addr, address token) external view returns (uint256);
+    // unclaimed
+    function claimed_reward(address addr, address token) external view returns (uint256);
 }
 
 /*  solhint-disable func-name-mixedcase, var-name-mixedcase */
--- src/interfaces/curve/IMinter.sol
+++ src/interfaces/curve/IMinter.sol
@@ -1,16 +1,10 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity =0.8.16;
 
-// See https://etherscan.io/address/0xd8b712d29381748dB89c36BCa0138d7c75866ddF#code
+// See https://etherscan.io/address/0xd061D61a4d941c39E5453435B6345Dc261C2fcE0#code
 /*  solhint-disable func-name-mixedcase, var-name-mixedcase */
 interface IMinter {
     function mint(address gauge) external;
-
-    // claimable_reward - claimed_reward = pending rewards
-    // Total rewards, claimed and unclaimed
-    function claimable_reward(address addr, address token) external view returns (uint256);
-    // unclaimed
-    function claimed_reward(address addr, address token) external view returns (uint256);
 }
 
 /*  solhint-disable func-name-mixedcase, var-name-mixedcase */
