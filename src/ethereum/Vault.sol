ea318f2ce912cca6d2bd998b2cfaec3203f27ad7
larryob <tosin@tryalpine.com>
2023-01-03 12:33:21 -0500

    Add detailed functions

--- src/ethereum/Vault.sol
+++ src/ethereum/Vault.sol
@@ -9,8 +9,9 @@ import {ERC20} from "solmate/src/tokens/ERC20.sol";
 
 import {AffineVault} from "../AffineVault.sol";
 import {Affine4626} from "../Affine4626.sol";
+import {DetailedShare} from "../both/Detailed.sol";
 
-contract Vault is AffineVault, Affine4626 {
+contract Vault is AffineVault, Affine4626, DetailedShare {
     function initialize(address _governance, address vaultAsset) external initializer {
         AffineVault.baseInitialize(_governance, ERC20(vaultAsset));
         __ERC20_init("USD Earn", "usdEarn");
@@ -24,5 +25,21 @@ contract Vault is AffineVault, Affine4626 {
     /// @notice See {IERC4626-totalAssets}
     function totalAssets() public view virtual override returns (uint256) {
         return vaultTVL() - lockedProfit();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                          DETAILED PRICE INFO
+    //////////////////////////////////////////////////////////////*/
+
+    function detailedTVL() external view override returns (Number memory tvl) {
+        tvl = Number({num: totalAssets(), decimals: _asset.decimals()});
+    }
+
+    function detailedPrice() external view override returns (Number memory price) {
+        price = Number({num: convertToAssets(10 ** decimals()), decimals: _asset.decimals()});
+    }
+
+    function detailedTotalSupply() external view override returns (Number memory supply) {
+        supply = Number({num: totalSupply(), decimals: decimals()});
     }
 }
--- src/polygon/L2Vault.sol
+++ src/polygon/L2Vault.sol
@@ -15,7 +15,7 @@ import {BaseRelayRecipient} from "@opengsn/contracts/src/BaseRelayRecipient.sol"
 
 import {BaseVault} from "../BaseVault.sol";
 import {L2BridgeEscrow} from "./L2BridgeEscrow.sol";
-import {DetailedShare} from "./Detailed.sol";
+import {DetailedShare} from "../both/Detailed.sol";
 import {L2WormholeRouter} from "./L2WormholeRouter.sol";
 import {IERC4626} from "../interfaces/IERC4626.sol";
 import {EmergencyWithdrawalQueue} from "./EmergencyWithdrawalQueue.sol";
