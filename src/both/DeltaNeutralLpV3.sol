33a7a049fff6236cee0c91f8c17c35ba4e03d4d7
larryob <tosin@multiplyr.xyz>
2023-02-02 20:38:33 -0500

    Add comment blocks

--- src/both/DeltaNeutralLpV3.sol
+++ src/both/DeltaNeutralLpV3.sol
@@ -72,59 +72,25 @@ contract DeltaNeutralLpV3 is AccessStrategy {
         borrowAsset.safeApprove(address(_lpManager), type(uint256).max);
     }
 
-    /// @notice Convert `borrowAsset` (e.g. MATIC) to `asset` (e.g. USDC)
-    function _borrowToAsset(uint256 amountB, uint256 clPrice) internal pure returns (uint256 assets) {
-        // The first divisition gets rid of the decimals of wmatic. The second converts dollars to usdc
-        // TODO: make this work for any set of decimals
-        assets = amountB.mulWadDown(clPrice) / 1e2;
+    /*//////////////////////////////////////////////////////////////
+                               DIVESTMENT
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev This strategy should be put at the end of the WQ so that we rarely divest from it. Divestment
+    /// ideally occurs when the strategy does not have an open position
+    function _divest(uint256 amount) internal override returns (uint256) {
+        // Totally unwind the position
+        if (!canStartNewPos) _endPosition(500);
+
+        uint256 amountToSend = Math.min(amount, balanceOfAsset());
+        asset.safeTransfer(address(vault), amountToSend);
+        // Return the given amount
+        return amountToSend;
     }
 
-    function _assetToBorrow(uint256 assets, uint256 clPrice) internal pure returns (uint256 borrows) {
-        borrows = (assets * 1e2).divWadDown(clPrice);
-    }
-
-    function _getPrice() internal view returns (uint256 priceOfBorrowAsset) {
-        (uint80 roundId, int256 price,, uint256 timestamp, uint80 answeredInRound) = borrowAssetFeed.latestRoundData();
-        require(price > 0, "Chainlink price <= 0");
-        require(answeredInRound >= roundId, "Chainlink stale data");
-        require(timestamp != 0, "Chainlink round not complete");
-
-        priceOfBorrowAsset = uint256(price);
-    }
-
-    function valueOfLpPosition() public view returns (uint256 assetsLp) {
-        (uint256 token0InLp, uint256 token1InLp) = _getTokensInLp();
-        (uint256 assetsInLp, uint256 borrowAssetsInLp) = _convertToAB(token0InLp, token1InLp);
-        assetsLp = assetsInLp + _borrowToAsset(borrowAssetsInLp, _getPrice());
-    }
-
-    function _getTokensInLp() internal view returns (uint256 amount0, uint256 amount1) {
-        if (lpLiquidity == 0) return (amount0, amount1);
-        (uint160 sqrtPriceX96,,,,,,) = pool.slot0();
-        (amount0, amount1) = positionValue.total(lpManager, lpId, sqrtPriceX96);
-    }
-
-    function positionFees() public view returns (uint256 assets, uint256 borrows) {
-        (uint256 token0Fees, uint256 token1Fees) = positionValue.fees(lpManager, lpId);
-        return _convertToAB(token0Fees, token1Fees);
-    }
-
-    function totalLockedValue() public view override returns (uint256) {
-        // The below are all in units of `asset`
-        // balanceOfAsset + balanceOfMatic + aToken value + Uni Lp value - debt
-        // lp tokens * (total assets) / total lp tokens
-        uint256 borrowPrice = _getPrice();
-        uint256 assetsMatic = _borrowToAsset(borrowAsset.balanceOf(address(this)), borrowPrice);
-
-        // Get value of uniswap lp position
-        uint256 assetsLp = valueOfLpPosition();
-
-        uint256 assetsDebt = _borrowToAsset(debtToken.balanceOf(address(this)), borrowPrice);
-        return balanceOfAsset() + assetsMatic + aToken.balanceOf(address(this)) + assetsLp - assetsDebt;
-    }
-
-    uint32 public currentPosition;
-    bool public canStartNewPos;
+    /*//////////////////////////////////////////////////////////////
+                          POSITION MANAGEMENT
+    //////////////////////////////////////////////////////////////*/
 
     event PositionStart(
         uint32 indexed position,
@@ -138,19 +104,8 @@ contract DeltaNeutralLpV3 is AccessStrategy {
         uint256 timestamp
     );
 
-    /// @notice The router used for swaps
-    ISwapRouter public immutable router;
-    INonfungiblePositionManager public immutable lpManager;
-    /// @notice The pool's fee. We need this to identify the pool.
-    uint24 public immutable poolFee;
-    IUniswapV3Pool public immutable pool;
-    /// @notice True if `asset` is pool.token0();
-    address immutable token0;
-    address immutable token1;
-    uint256 public lpId;
-    uint128 public lpLiquidity;
-    /// @notice A wrapper around the positionValue lib (written in solidity 0.7)
-    IUniPositionValue public immutable positionValue;
+    uint32 public currentPosition;
+    bool public canStartNewPos;
 
     /// @notice The asset we want to borrow, e.g. WMATIC
     ERC20 public immutable borrowAsset;
@@ -159,9 +114,6 @@ contract DeltaNeutralLpV3 is AccessStrategy {
     ERC20 public immutable debtToken;
     /// @notice The asset we get deposit `asset` into aave
     ERC20 public immutable aToken;
-
-    /// @notice Gives ratio of vault asset to borrow asset, e.g. WMATIC/USD (assuming usdc = usd)
-    AggregatorV3Interface immutable borrowAssetFeed;
 
     function startPosition(int24 tickLow, int24 tickHigh, uint256 slippageToleranceBps)
         external
@@ -208,18 +160,6 @@ contract DeltaNeutralLpV3 is AccessStrategy {
             borrowsToUni: borrowsToUni,
             timestamp: block.timestamp
         });
-    }
-
-    /// @dev This strategy should be put at the end of the WQ so that we rarely divest from it. Divestment
-    /// ideally occurs when the strategy does not have an open position
-    function _divest(uint256 amount) internal override returns (uint256) {
-        // Totally unwind the position
-        if (!canStartNewPos) _endPosition(500);
-
-        uint256 amountToSend = Math.min(amount, balanceOfAsset());
-        asset.safeTransfer(address(vault), amountToSend);
-        // Return the given amount
-        return amountToSend;
     }
 
     /**
@@ -302,22 +242,6 @@ contract DeltaNeutralLpV3 is AccessStrategy {
         );
     }
 
-    /// @dev Another function to avoid stack too deep error. Via-ir compilation takes too long.
-    function _getBorrowSpotPrice() internal view returns (uint256 price) {
-        (uint160 sqrtPriceX96,,,,,,) = pool.slot0();
-
-        // We are converting "one" of `borrowAsset` into some amount of `asset`.
-        uint256 oneBorrow = 10 ** borrowAsset.decimals();
-        if (address(asset) == token0) {
-            // eth_amount / (eth_usdc ratio)  = eth_amount * usdc/eth ratio = usdc amount
-            price = (oneBorrow << 192) / (uint256(sqrtPriceX96) ** 2);
-        } else {
-            // eth_amount * usdc/eth ratio = usdc amount
-            price = (oneBorrow * uint256(sqrtPriceX96) ** 2) >> 192;
-        }
-        return price;
-    }
-
     function _emitEnd(
         uint256 amount0FromUni,
         uint256 amount1FromUni,
@@ -350,18 +274,25 @@ contract DeltaNeutralLpV3 is AccessStrategy {
         }
     }
 
-    /// @dev Given two numbers in AB (assets, borrows) format, convert to Uniswap's token0, token1 format
-    function _convertTo01(uint256 assets, uint256 borrowAssets) internal view returns (uint256, uint256) {
-        if (address(asset) == token0) return (assets, borrowAssets);
-        else return (borrowAssets, assets);
-    }
+    /*//////////////////////////////////////////////////////////////
+                                UNISWAP
+    //////////////////////////////////////////////////////////////*/
 
-    /// @dev Given two numbers in 01 (token0, token1) format, convert to our AB format (assets, borrows). This will just flip
-    /// the numbers if asset != token0.
-    function _convertToAB(uint256 amount0, uint256 amount1) internal view returns (uint256, uint256) {
-        return _convertTo01(amount0, amount1);
-    }
+    /// @notice The router used for swaps
+    ISwapRouter public immutable router;
+    INonfungiblePositionManager public immutable lpManager;
+    /// @notice The pool's fee. We need this to identify the pool.
+    uint24 public immutable poolFee;
+    IUniswapV3Pool public immutable pool;
+    /// @notice True if `asset` is pool.token0();
+    address immutable token0;
+    address immutable token1;
+    uint256 public lpId;
+    uint128 public lpLiquidity;
+    /// @notice A wrapper around the positionValue lib (written in solidity 0.7)
+    IUniPositionValue public immutable positionValue;
 
+    /// @dev Another function to avoid stack too deep error. Via-ir compilation takes too long.
     function _addLiquidity(
         uint256 amountA,
         uint256 amountB,
@@ -414,6 +345,12 @@ contract DeltaNeutralLpV3 is AccessStrategy {
         (amount0FromLiq, amount1FromLiq) = _collectFees();
     }
 
+    function _getTokensInLp() internal view returns (uint256 amount0, uint256 amount1) {
+        if (lpLiquidity == 0) return (amount0, amount1);
+        (uint160 sqrtPriceX96,,,,,,) = pool.slot0();
+        (amount0, amount1) = positionValue.total(lpManager, lpId, sqrtPriceX96);
+    }
+
     function _collectFees() internal returns (uint256 amount0, uint256 amount1) {
         // This will actually transfer the tokens owed after decreasing the liquidity
         INonfungiblePositionManager.CollectParams memory params = INonfungiblePositionManager.CollectParams({
@@ -424,15 +361,6 @@ contract DeltaNeutralLpV3 is AccessStrategy {
         });
 
         (amount0, amount1) = lpManager.collect(params);
-    }
-
-    function _convertAmounts(ERC20 from, uint256 amountFrom) internal view returns (uint256 amountTo) {
-        uint256 borrowPrice = _getPrice();
-        if (address(from) == address(asset)) {
-            amountTo = _assetToBorrow(amountFrom, borrowPrice);
-        } else {
-            amountTo = _borrowToAsset(amountFrom, borrowPrice);
-        }
     }
 
     function _swapExactSingle(ERC20 from, ERC20 to, uint256 amountIn, uint256 slippageBps)
@@ -474,5 +402,101 @@ contract DeltaNeutralLpV3 is AccessStrategy {
 
         sold = router.exactOutputSingle(params);
         received = amountOut;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                           FORMAT CONVERSIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Given two numbers in AB (assets, borrows) format, convert to Uniswap's token0, token1 format
+    function _convertTo01(uint256 assets, uint256 borrowAssets) internal view returns (uint256, uint256) {
+        if (address(asset) == token0) return (assets, borrowAssets);
+        else return (borrowAssets, assets);
+    }
+
+    /// @dev Given two numbers in 01 (token0, token1) format, convert to our AB format (assets, borrows). This will just flip
+    /// the numbers if asset != token0.
+    function _convertToAB(uint256 amount0, uint256 amount1) internal view returns (uint256, uint256) {
+        return _convertTo01(amount0, amount1);
+    }
+
+    function _convertAmounts(ERC20 from, uint256 amountFrom) internal view returns (uint256 amountTo) {
+        uint256 borrowPrice = _getPrice();
+        if (address(from) == address(asset)) {
+            amountTo = _assetToBorrow(amountFrom, borrowPrice);
+        } else {
+            amountTo = _borrowToAsset(amountFrom, borrowPrice);
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             EXCHAGE RATES
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Gives ratio of vault asset to borrow asset, e.g. WMATIC/USD (assuming usdc = usd)
+    AggregatorV3Interface immutable borrowAssetFeed;
+
+    /// @notice Convert `borrowAsset` (e.g. MATIC) to `asset` (e.g. USDC)
+    function _borrowToAsset(uint256 amountB, uint256 clPrice) internal pure returns (uint256 assets) {
+        // The first division gets rid of the decimals of wmatic. The second converts dollars to usdc
+        // TODO: make this work for any set of decimals
+        assets = amountB.mulWadDown(clPrice) / 1e2;
+    }
+
+    function _assetToBorrow(uint256 assets, uint256 clPrice) internal pure returns (uint256 borrows) {
+        borrows = (assets * 1e2).divWadDown(clPrice);
+    }
+
+    function _getPrice() internal view returns (uint256 priceOfBorrowAsset) {
+        (uint80 roundId, int256 price,, uint256 timestamp, uint80 answeredInRound) = borrowAssetFeed.latestRoundData();
+        require(price > 0, "Chainlink price <= 0");
+        require(answeredInRound >= roundId, "Chainlink stale data");
+        require(timestamp != 0, "Chainlink round not complete");
+
+        priceOfBorrowAsset = uint256(price);
+    }
+
+    function _getBorrowSpotPrice() internal view returns (uint256 price) {
+        (uint160 sqrtPriceX96,,,,,,) = pool.slot0();
+
+        // We are converting "one" of `borrowAsset` into some amount of `asset`.
+        uint256 oneBorrow = 10 ** borrowAsset.decimals();
+        if (address(asset) == token0) {
+            // eth_amount / (eth_usdc ratio)  = eth_amount * usdc/eth ratio = usdc amount
+            price = (oneBorrow << 192) / (uint256(sqrtPriceX96) ** 2);
+        } else {
+            // eth_amount * usdc/eth ratio = usdc amount
+            price = (oneBorrow * uint256(sqrtPriceX96) ** 2) >> 192;
+        }
+        return price;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             TVL ESTIMATION
+    //////////////////////////////////////////////////////////////*/
+
+    function totalLockedValue() public view override returns (uint256) {
+        // The below are all in units of `asset`
+        // balanceOfAsset + balanceOfMatic + aToken value + Uni Lp value - debt
+        // lp tokens * (total assets) / total lp tokens
+        uint256 borrowPrice = _getPrice();
+        uint256 assetsMatic = _borrowToAsset(borrowAsset.balanceOf(address(this)), borrowPrice);
+
+        // Get value of uniswap lp position
+        uint256 assetsLp = valueOfLpPosition();
+
+        uint256 assetsDebt = _borrowToAsset(debtToken.balanceOf(address(this)), borrowPrice);
+        return balanceOfAsset() + assetsMatic + aToken.balanceOf(address(this)) + assetsLp - assetsDebt;
+    }
+
+    function valueOfLpPosition() public view returns (uint256 assetsLp) {
+        (uint256 token0InLp, uint256 token1InLp) = _getTokensInLp();
+        (uint256 assetsInLp, uint256 borrowAssetsInLp) = _convertToAB(token0InLp, token1InLp);
+        assetsLp = assetsInLp + _borrowToAsset(borrowAssetsInLp, _getPrice());
+    }
+
+    function positionFees() public view returns (uint256 assets, uint256 borrows) {
+        (uint256 token0Fees, uint256 token1Fees) = positionValue.fees(lpManager, lpId);
+        return _convertToAB(token0Fees, token1Fees);
     }
 }
