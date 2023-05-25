d99a49300ada5e2f1c662360b6c43b9ffe1a3f91
larryob <tosin@multiplyr.xyz>
2023-05-25 15:01:27 -0400

    Add leverage param and deploy script

--- src/strategies/Staking.sol
+++ src/strategies/Staking.sol
@@ -23,15 +23,37 @@ contract StakingExp is IFlashLoanRecipient {
     ERC20 public constant WSTETH = ERC20(0xBe9895146f7AF43049ca1c1AE358B0541Ea49704); // actually coinbase Eth
     IPool public constant AAVE = IPool(0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2);
 
-    constructor(IBalancerVault _vault) {
+    /// @dev Aave deposit receipt token.
+    ERC20 public immutable aToken;
+    /// @dev Aave debt receipt token.
+    ERC20 public immutable debtToken;
+
+    address public immutable owner;
+
+    /// @dev We need this to receive ETH when calling WETH.withdraw()
+    receive() external payable {}
+
+    constructor(IBalancerVault _vault, address _owner) {
         vault = _vault;
+        owner = _owner;
+
+        aToken = ERC20(AAVE.getReserveData(address(WSTETH)).aTokenAddress);
+        debtToken = ERC20(AAVE.getReserveData(address(WETH)).variableDebtTokenAddress);
+
         WETH.safeApprove(address(ROUTER), type(uint256).max);
         WSTETH.safeApprove(address(ROUTER), type(uint256).max);
 
         WSTETH.safeApprove(address(AAVE), type(uint256).max);
+        aToken.safeApprove(address(AAVE), type(uint256).max);
+        WETH.safeApprove(address(AAVE), type(uint256).max);
     }
 
-    function startPosition(uint256 size, uint256 slippageBps) external {
+    uint256 public lastPosSize;
+    int256 public lastPosPnl;
+
+    /// @dev Always begin with raw ETH, and 0 WETH
+    function startPosition(uint256 size, uint256 slippageBps, uint256 leverage) external {
+        require(msg.sender == owner, "Staking: only owner can call");
         // Make sure balance is at least 1.005 * the intended deposit size
         uint256 ethNeeded = size + size.mulDivUp(slippageBps * 10, 10_000);
         require(ethNeeded <= address(this).balance, "Staking: insufficient balance");
@@ -39,20 +61,76 @@ contract StakingExp is IFlashLoanRecipient {
         ERC20[] memory tokens = new ERC20[](1);
         tokens[0] = WETH;
         uint256[] memory amounts = new uint256[](1);
-        amounts[0] = size * 10;
-        IWETH(address(WETH)).deposit{value: size}();
+        amounts[0] = size * leverage;
+        IWETH(address(WETH)).deposit{value: ethNeeded}();
+        lastPosSize = ethNeeded;
 
-        vault.flashLoan(IFlashLoanRecipient(address(this)), tokens, amounts, abi.encodePacked(size));
+        vault.flashLoan(IFlashLoanRecipient(address(this)), tokens, amounts, abi.encode(Loan.open, size));
+    }
+
+    enum Loan {
+        open,
+        close
+    }
+
+    function endPosition() external {
+        require(msg.sender == owner, "Staking: only owner can call");
+        // Take loan for amount of debt from aave
+        ERC20[] memory tokens = new ERC20[](1);
+        tokens[0] = WETH;
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = debtToken.balanceOf(address(this));
+        vault.flashLoan(IFlashLoanRecipient(address(this)), tokens, amounts, abi.encode(Loan.close, uint256(0)));
+    }
+
+    function _endPosition(uint256 amount) internal {
+        // Withdraw wstEth from aave
+
+        AAVE.repay({
+            asset: address(WETH),
+            amount: debtToken.balanceOf(address(this)),
+            interestRateMode: 2,
+            onBehalfOf: address(this)
+        });
+        AAVE.withdraw({asset: address(WSTETH), amount: aToken.balanceOf(address(this)), to: address(this)});
+
+        // Swap wstETH to WETH
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
+            tokenIn: address(WSTETH),
+            tokenOut: address(WETH),
+            fee: 500,
+            recipient: address(this),
+            deadline: block.timestamp,
+            amountIn: WSTETH.balanceOf(address(this)),
+            amountOutMinimum: 0, // TODO: slippage protection
+            sqrtPriceLimitX96: 0
+        });
+        ROUTER.exactInputSingle(params);
+
+        // Pay back flash loan using WETH
+        WETH.safeTransfer(address(vault), amount);
+
+        lastPosPnl = int256(WETH.balanceOf(address(this))) - int256(lastPosSize);
+
+        // Unwrap all WETH
+        IWETH(address(WETH)).withdraw(WETH.balanceOf(address(this)));
     }
 
     function receiveFlashLoan(
         ERC20[] memory tokens,
         uint256[] memory amounts,
-        uint256[] memory, /*feeAmounts */
+        uint256[] memory, /* feeAmounts */
         bytes memory userData
     ) external override {
+        require(msg.sender == address(vault), "Staking: only vault");
+
         ERC20 asset = tokens[0];
         uint256 amount = amounts[0];
+
+        (Loan loanType, uint256 posSize) = abi.decode(userData, (Loan, uint256));
+        if (loanType == Loan.close) {
+            return _endPosition(amount);
+        }
 
         // WETH to wstETH (via trade)
         // Uniswap pool is pretty big: https://info.uniswap.org/#/pools/0x840deeef2f115cf50da625f7368c24af6fe74410
@@ -79,7 +157,6 @@ contract StakingExp is IFlashLoanRecipient {
         // Borrow Eth. TODO: consider converting WSTETH to ETH using oracle prices
         // TODO: make sure collat ratio is safe
         AAVE.setUserEMode(1); // Allow up to 90% borrow
-        uint256 posSize = abi.decode(userData, (uint256));
         AAVE.borrow({
             asset: address(WETH),
             amount: amount - posSize,
@@ -90,5 +167,11 @@ contract StakingExp is IFlashLoanRecipient {
 
         // Payback loan
         asset.safeTransfer(address(vault), amount);
+    }
+
+    function withdrawEth(uint256 amount, address to) external {
+        require(msg.sender == owner, "Staking: only owner can call");
+        (bool success,) = to.call{value: amount}("");
+        require(success, "Staking: transfer failed");
     }
 }
--- src/test/Staking.t.sol
+++ src/test/Staking.t.sol
@@ -7,21 +7,32 @@ import {stdStorage, StdStorage} from "forge-std/Test.sol";
 import {ERC20} from "solmate/src/tokens/ERC20.sol";
 import {StakingExp, IBalancerVault, IFlashLoanRecipient} from "src/strategies/Staking.sol";
 
+import {console} from "forge-std/console.sol";
+
 contract StakingTest is TestPlus {
     StakingExp staking;
 
+    receive() external payable {}
+
     function setUp() public {
-        forkEth();
-        staking = new StakingExp(IBalancerVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8));
+        // forkEth();
+        vm.createSelectFork("ethereum", 17_337_424);
+
+        staking = new StakingExp(IBalancerVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8), address(this));
     }
 
     function testFlashLoan() public {
         vm.deal(address(staking), 1.005 ether);
-        staking.startPosition(1 ether, 5);
-        // ERC20[] memory tokens = new ERC20[](1);
-        // tokens[0] = ERC20(0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619);
-        // uint256[] memory amounts = new uint256[](1);
-        // amounts[0] = 1.005 ether;
-        // staking.flashLoan(tokens, amounts, "");
+        staking.startPosition(1 ether, 5, 9);
+        staking.endPosition();
+
+        console.logInt(staking.lastPosPnl());
+    }
+
+    function testWithdraw() public {
+        vm.deal(address(staking), 1 ether);
+        staking.withdrawEth(1 ether, alice);
+        assertEq(alice.balance, 1 ether);
+        assertEq(address(staking).balance, 0);
     }
 }
