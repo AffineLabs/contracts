9c31543b5cc40426e63f3b8b518acf734b61c951
larryob <tosin@multiplyr.xyz>
2023-08-07 15:38:46 -0400

    Add more deploy scripts

--- script/upgrades/StrategyVaultV2.s.sol
+++ script/upgrades/StrategyVaultV2.s.sol
@@ -11,6 +11,17 @@ contract DegenVaultV2 is StrategyVaultV2 {
     }
 }
 
+contract DegenVaultV2Eth is StrategyVaultV2 {
+     function _initialShareDecimals() internal pure override returns (uint8) {
+        return 10;
+    }
+}
+
+contract HighYieldLpVaultEth is StrategyVaultV2 {
+     function _initialShareDecimals() internal pure override returns (uint8) {
+        return 10;
+    }
+}
 
 contract DeployDegen is Script {
     function run() external {
@@ -18,6 +29,22 @@ contract DeployDegen is Script {
         vm.startBroadcast(deployer);
 
         DegenVaultV2 vault = new DegenVaultV2();
+        console.log("new implementation address: ", address(vault));
+    }
+
+    function runDegenEth() external {
+        (address deployer,) = deriveRememberKey(vm.envString("MNEMONIC"), 0);
+        vm.startBroadcast(deployer);
+
+        DegenVaultV2Eth vault = new DegenVaultV2Eth();
+        console.log("new implementation address: ", address(vault));
+    }
+
+    function runHighYieldLpEth() external {
+        (address deployer,) = deriveRememberKey(vm.envString("MNEMONIC"), 0);
+        vm.startBroadcast(deployer);
+
+        HighYieldLpVaultEth vault = new HighYieldLpVaultEth();
         console.log("new implementation address: ", address(vault));
     }
 }
