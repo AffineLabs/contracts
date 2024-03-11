19ac3f5a8b39bb47b1da6df689fb45d2762f3890
maruf <maruf@tryalpine.com>
2024-03-11 12:02:05 -0500

    polygon aave v3 dep script

--- script/AAVEV3Strategy.s.sol
+++ script/AAVEV3Strategy.s.sol
@@ -17,4 +17,18 @@ contract Deploy is Script {
         AaveV3Strategy strategy = new AaveV3Strategy(vault, IPool(0xA238Dd80C259a72e81d7e4664a9801593F98d1c5));
         require(strategy.vault() == vault);
     }
+
+    function runPoly() external {
+        (address deployer,) = deriveRememberKey(vm.envString("MNEMONIC"), 0);
+        vm.startBroadcast(deployer);
+
+        console2.log("dep %s", deployer);
+
+        AffineVault vault = AffineVault(0x829363736a5A9080e05549Db6d1271f070a7e224);
+        AaveV3Strategy strategy = new AaveV3Strategy(vault, IPool(0x794a61358D6845594F94dc1DB02A252b5b4814aD));
+
+        console2.log("strat %s", address(strategy));
+        require(strategy.vault() == vault);
+        require(address(strategy.asset()) == address(vault.asset()));
+    }
 }
