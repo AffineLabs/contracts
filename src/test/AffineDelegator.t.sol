885b1dd5985c7edd866787e8231c7da6f519989c
omurshid <osama.murshid@outlook.com>
2024-04-30 11:21:32 -0400

    changed tvl to shares

--- src/test/AffineDelegator.t.sol
+++ src/test/AffineDelegator.t.sol
@@ -8,6 +8,7 @@ import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.s
 import {ERC20} from "solmate/src/tokens/ERC20.sol";
 import {LevMaticXLoopStrategy, AffineVault, FixedPointMathLib} from "src/strategies/LevMaticXLoopStrategy.sol";
 import {AffineDelegator, WithdrawalInfo, IStrategy} from "src/vaults/restaking/AffineDelegator.sol";
+import {IStEth} from "src/interfaces/lido/IStEth.sol";
 
 import {console2} from "forge-std/console2.sol";
 
@@ -15,10 +16,11 @@ contract AffineDelegatorTest is TestPlus {
     uint256 init_assets = 10e18; // 10 stETH
     address vault;
     AffineDelegator delegator;
+    address operator = 0xDbEd88D83176316fc46797B43aDeE927Dc2ff2F5;
 
     receive() external payable {}
 
-    ERC20 public asset = ERC20((0xae78736Cd615f374D3085123A210448E74Fc6393)); // rocket pool stETH
+    ERC20 public asset = ERC20((0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84)); // rocket pool stETH
 
 
     function setUp() public {
@@ -26,54 +28,63 @@ contract AffineDelegatorTest is TestPlus {
         vm.createSelectFork("ethereum");
         vault = alice;
 
-        AffineDelegator impl = new AffineDelegator();
+        delegator = new AffineDelegator(governance, vault, operator);
 
-        bytes memory initData = abi.encodeCall(AffineDelegator.initialize, (governance, vault));
-        ERC1967Proxy proxy = new ERC1967Proxy(address(impl), initData);
+        // bytes memory initData = abi.encodeCall(AffineDelegator.initialize, (governance, vault));
+        // ERC1967Proxy proxy = new ERC1967Proxy(address(impl), initData);
 
-        delegator = AffineDelegator(address(proxy));
+        // delegator = AffineDelegator(address(proxy));
+    }
+
+    function _getAsset(address to, uint256 amount) internal returns (uint256) {
+        deal(to, amount);
+        vm.prank(to);
+        IStEth(address(asset)).submit{value: amount}(address(0));
+        return asset.balanceOf(to);
     }
 
     function testDelegate() public {
-        deal(address(asset), alice, init_assets);
-        IStrategy stEthStrategy = IStrategy(0x1BeE69b7dFFfA4E2d53C2a2Df135C388AD25dCD2);
+        // deal(address(asset), alice, init_assets);
+        uint256 stEthAmount = _getAsset(alice, init_assets);
+        IStrategy stEthStrategy = IStrategy(0x93c4b944D05dfe6df7645A86cd2206016c51564D);
         vm.startPrank(alice);
 
         // delegate
-        asset.approve(address(delegator), init_assets);
-        delegator.delegate(init_assets);
-        assertEq(delegator.totalLockedValue(), init_assets);
-        assertEq(asset.balanceOf(alice), 0);
-        
+        asset.approve(address(delegator), stEthAmount);
+        delegator.delegate(stEthAmount);
+        uint256 sharesAmount = stEthStrategy.underlyingToShares(stEthAmount);
+        assertApproxEqAbs(delegator.totalLockedValue(), sharesAmount, 10);
+        assertApproxEqAbs(asset.balanceOf(alice), 0, 10);
 
         // request withdrawal
-        delegator.requestWithdrawal(init_assets);
+        delegator.requestWithdrawal(stEthAmount);
         uint256 blockNum = block.number;
-        assertEq(delegator.totalLockedValue(), init_assets);
+        
+        assertApproxEqAbs(delegator.totalLockedValue(), sharesAmount, 10);
         vm.roll(block.number + 1000000);
 
         // complete withdrawal
         WithdrawalInfo[] memory params = new WithdrawalInfo[](1);
         uint256[] memory shares = new uint256[](1);
-        shares[0] = stEthStrategy.underlyingToShares(init_assets);
+        shares[0] = stEthStrategy.underlyingToShares(stEthAmount);
         address[] memory strategies = new address[](1);
         strategies[0] = address(stEthStrategy);
 
         params[0] = WithdrawalInfo({
             staker: address(delegator),
-            delegatedTo: 0xDbEd88D83176316fc46797B43aDeE927Dc2ff2F5,
+            delegatedTo: operator,
             withdrawer: address(delegator),
             nonce: 0,
             startBlock: uint32(blockNum),
             strategies: strategies,
             shares: shares
         });
-        
+ 
         delegator.completeWithdrawalRequest(params);
         delegator.withdraw();
 
         assertApproxEqAbs(delegator.totalLockedValue(), 0, 10);
-        assertApproxEqAbs(asset.balanceOf(alice), init_assets, 10);
+        assertApproxEqAbs(asset.balanceOf(alice), stEthAmount, 10);
     }
 
 }
