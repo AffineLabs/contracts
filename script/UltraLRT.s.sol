34158cfaf94a9e8eef617a674ad7380ca9f9af29
maruf <maruf@tryalpine.com>
2024-05-28 11:02:34 -0500

    Add testnet script

--- script/UltraLRT.s.sol
+++ script/UltraLRT.s.sol
@@ -15,16 +15,17 @@ import {WithdrawalEscrowV2} from "src/vaults/restaking/WithdrawalEscrowV2.sol";
 import {AffineDelegator, WithdrawalInfo, IStrategy} from "src/vaults/restaking/AffineDelegator.sol";
 import {DelegatorBeacon} from "src/vaults/restaking/DelegatorBeacon.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {DelegatorFactory} from "src/vaults/restaking/DelegatorFactory.sol";
 
 contract Deploy is Script {
-    ERC20 asset = ERC20(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84);
-    address operator = 0xDbEd88D83176316fc46797B43aDeE927Dc2ff2F5;
-    IStrategy stEthStrategy = IStrategy(0x93c4b944D05dfe6df7645A86cd2206016c51564D);
-    uint256 initAssets;
-
     function run() external {
         (address deployer,) = deriveRememberKey(vm.envString("MNEMONIC"), 0);
         vm.startBroadcast(deployer);
+
+        ERC20 asset = ERC20(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84);
+        address operator = 0xDbEd88D83176316fc46797B43aDeE927Dc2ff2F5;
+        // IStrategy stEthStrategy = IStrategy(0x93c4b944D05dfe6df7645A86cd2206016c51564D);
+
         UltraLRT impl = new UltraLRT();
         // delegator implementation
         AffineDelegator delegatorImpl = new AffineDelegator();
@@ -38,9 +39,6 @@ contract Deploy is Script {
         // upgradeable vault
         UltraLRT vault = UltraLRT(address(proxy));
 
-        initAssets = 10 ** asset.decimals();
-        initAssets *= 100;
-
         // add withdrawal escrow
         WithdrawalEscrowV2 escrow = new WithdrawalEscrowV2(vault);
         vault.setWithdrawalEscrow(escrow);
@@ -50,15 +48,73 @@ contract Deploy is Script {
         console2.log("escrow address %s", address(escrow));
     }
 
-    function run2() public {
+    function _start() internal returns (address) {
         (address deployer,) = deriveRememberKey(vm.envString("MNEMONIC"), 0);
         vm.startBroadcast(deployer);
 
-        UltraLRT vault = UltraLRT(0xeA8AE08513f8230cAA8d031D28cB4Ac8CE720c68);
-        asset.approve(address(vault), 10 * 1e18);
-        vault.deposit(10 * 1e18, deployer);
+        console2.log("deployer gov %s", deployer);
+        return deployer;
+    }
 
-        console2.log("dep shares %s", vault.balanceOf(deployer));
-        console2.log("dep assets %s", vault.previewRedeem(vault.balanceOf(deployer)));
+    function runHoleSky() external {
+        address deployer = _start();
+
+        ERC20 asset = ERC20(0x3F1c547b21f65e10480dE3ad8E19fAAC46C95034); // holesky
+        // address operator = 0x0a3e3d83C99B27cA7540720b54105C79Cd58dbdD; // holesky
+        // IStrategy stEthStrategy = IStrategy(0x7D704507b76571a51d9caE8AdDAbBFd0ba0e63d3); // holesky
+
+        DelegatorBeacon beacon = DelegatorBeacon(0x75019A4BBCAa8eDA648Af4eeC11290839bC5FcE9);
+
+        UltraLRT impl = UltraLRT(0x7331aD312BAF6CFb127a84DbA077b72295cFEB28);
+        // delegator implementation
+
+        // initialization data
+        bytes memory initData =
+            abi.encodeCall(UltraLRT.initialize, (deployer, address(asset), address(beacon), "ultraETH", "uEth"));
+        console2.logBytes(initData);
+        // proxy
+        ERC1967Proxy proxy = new ERC1967Proxy(address(impl), initData);
+        // upgradeable vault
+        UltraLRT vault = UltraLRT(address(proxy));
+
+        console2.log("vault address %s", address(vault));
+    }
+
+    function runHoleSkyWEscrow() public {
+        // add withdrawal escrow
+        _start();
+        UltraLRT vault = UltraLRT(0x3b07A1A5de80f9b22DE0EC6C44C6E59DDc1C5f41);
+        WithdrawalEscrowV2 escrow = new WithdrawalEscrowV2(vault);
+        vault.setWithdrawalEscrow(escrow);
+
+        console2.log("escrow address %s", address(escrow));
+        // // delegator factory
+        // DelegatorFactory dFactory = new DelegatorFactory(address(vault));
+        // vault.setDelegatorFactory(address(dFactory));
+    }
+
+    function runHoleSkyDFactory() public {
+        // add withdrawal escrow
+        _start();
+        UltraLRT vault = UltraLRT(0x3b07A1A5de80f9b22DE0EC6C44C6E59DDc1C5f41);
+
+        // delegator factory
+        DelegatorFactory dFactory = new DelegatorFactory(address(vault));
+        vault.setDelegatorFactory(address(dFactory));
+
+        console2.log("Factory add %s", address(dFactory));
+    }
+
+    function runDeposit() public {
+        // add withdrawal escrow
+        _start();
+        UltraLRT vault = UltraLRT(0x3b07A1A5de80f9b22DE0EC6C44C6E59DDc1C5f41);
+        ERC20 asset = ERC20(0x3F1c547b21f65e10480dE3ad8E19fAAC46C95034);
+
+        uint256 assets = asset.balanceOf(address(vault));
+        // delegator factory
+        // asset.approve(address(vault), assets);
+        console2.log("assets %s", assets);
+        vault.delegateToDelegator(address(vault.delegatorQueue(0)), asset.balanceOf(address(vault)));
     }
 }
