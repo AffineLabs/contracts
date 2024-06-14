bd99388f09be85e010f01f5c12a88d06cfa06334
maruf <maruf@tryalpine.com>
2024-06-13 19:15:15 -0500

    Add permit2 for asset transfer

--- src/vaults/restaking/UtraLRTRouter.sol
+++ src/vaults/restaking/UtraLRTRouter.sol
@@ -12,6 +12,9 @@ import {IWETH} from "src/interfaces/IWETH.sol";
 import {IStEth} from "src/interfaces/lido/IStEth.sol";
 import {IWSTETH} from "src/interfaces/lido/IWSTETH.sol";
 
+import {IPermit2} from "src/interfaces/permit2/IPermit2.sol";
+import {ISignatureTransfer} from "src/interfaces/permit2/ISignatureTransfer.sol";
+
 // import {SafeTransferLib} from "solmate/src/utils/SafeTransferLib.sol";
 
 import {UltraLRT} from "src/vaults/restaking/UltraLRT.sol";
@@ -20,12 +23,17 @@ contract UltraLRTRouter is UUPSUpgradeable, PausableUpgradeable, AffineGovernabl
     IWETH public weth;
     IStEth public stEth;
     IWSTETH public wStEth;
+    IPermit2 public permit2;
 
-    function initialize(address _governance, address _weth, address _stEth, address _wStEth) external initializer {
+    function initialize(address _governance, address _weth, address _stEth, address _wStEth, address _permit2)
+        external
+        initializer
+    {
         governance = _governance;
         weth = IWETH(_weth);
         stEth = IStEth(_stEth);
         wStEth = IWSTETH(_wStEth);
+        permit2 = IPermit2(_permit2);
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyGovernance {}
@@ -39,24 +47,70 @@ contract UltraLRTRouter is UUPSUpgradeable, PausableUpgradeable, AffineGovernabl
         _processDepositFromStEth(amount, vault, to);
     }
 
-    function depositWeth(uint256 amount, address vault, address to) external {
-        //TODO receive from permit2
-        //transfer
-        weth.transferFrom(msg.sender, address(this), amount);
+    function _receiveAssetFromThroughPermit2(
+        address token,
+        uint256 amount,
+        uint256 nonce,
+        uint256 deadline,
+        bytes calldata signature
+    ) internal {
+        permit2.permitTransferFrom(
+            // The permit message.
+            ISignatureTransfer.PermitTransferFrom({
+                permitted: ISignatureTransfer.TokenPermissions({token: token, amount: amount}),
+                nonce: nonce,
+                deadline: deadline
+            }),
+            // The transfer recipient and amount.
+            ISignatureTransfer.SignatureTransferDetails({to: address(this), requestedAmount: amount}),
+            // The owner of the tokens, which must also be
+            // the signer of the message, otherwise this call
+            // will fail.
+            msg.sender,
+            // The packed signature that was the result of signing
+            // the EIP712 hash of `permit`.
+            signature
+        );
+    }
+
+    function depositWeth(
+        uint256 amount,
+        address vault,
+        address to,
+        uint256 nonce,
+        uint256 deadline,
+        bytes calldata signature
+    ) external {
+        _receiveAssetFromThroughPermit2(address(weth), amount, nonce, deadline, signature);
+        // weth.transferFrom(msg.sender, address(this), amount);
         weth.withdraw(amount);
         depositNative(amount, vault, to);
     }
 
-    function depositStEth(uint256 amount, address vault, address to) external {
-        //TODO receive from permit2
-        //transfer
-
-        stEth.transferFrom(msg.sender, address(this), amount);
+    function depositStEth(
+        uint256 amount,
+        address vault,
+        address to,
+        uint256 nonce,
+        uint256 deadline,
+        bytes calldata signature
+    ) external {
+        _receiveAssetFromThroughPermit2(address(stEth), amount, nonce, deadline, signature);
+        // stEth.transferFrom(msg.sender, address(this), amount);
         _processDepositFromStEth(amount, vault, to);
     }
 
-    function depositWStEth(uint256 amount, address vault, address to) external {
-        wStEth.transferFrom(msg.sender, address(this), amount);
+    function depositWStEth(
+        uint256 amount,
+        address vault,
+        address to,
+        uint256 nonce,
+        uint256 deadline,
+        bytes calldata signature
+    ) external {
+        _receiveAssetFromThroughPermit2(address(stEth), amount, nonce, deadline, signature);
+
+        // wStEth.transferFrom(msg.sender, address(this), amount);
         if (UltraLRT(vault).asset() == address(wStEth)) {
             _depositWStEthToVault(amount, vault, to);
         } else if (UltraLRT(vault).asset() == address(stEth)) {
